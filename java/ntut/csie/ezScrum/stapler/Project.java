5572e60ed332fb88e50f31cfef4b910302e2a17f
Zam <love8879201@gmail.com>
2014-04-29 14:58:05 +0800

    [更新] protocol jar更新為可以配合Project Left Tree Plug-in [更新] 將架構改成支援Project Left Tree

--- java/ntut/csie/ezScrum/stapler/Project.java
+++ java/ntut/csie/ezScrum/stapler/Project.java
@@ -1,6 +1,7 @@
 package ntut.csie.ezScrum.stapler;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
@@ -8,6 +9,8 @@ import ntut.csie.ezScrum.plugin.util.PluginConfigManager;
 import ntut.csie.ezScrum.web.support.SessionManager;
 import ntut.csie.jcis.resource.core.IProject;
 import ntut.csie.protocal.PluginConfig;
+import ntut.csie.ui.protocol.EzScrumUI;
+import ntut.csie.ui.protocol.ProjectUI;
 
 import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.StaplerResponse;
@@ -17,66 +20,114 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonParser;
 
 public class Project {
-	private String projectName;
-	public Project( String projectName ){
-		this.projectName = projectName;
+	private String mProjectName;
+
+	public Project(String projectName) {
+		this.mProjectName = projectName;
 	}
-    public void doSetProjectPluginConfig( StaplerRequest request, StaplerResponse response ){
-    	//get plugin config page string
-    	String pluginConfigJSONArrayString = request.getParameter("pluginConfigJSONArrayString");
-    	PluginConfigManager pluginConfigModifier = new PluginConfigManager( this.projectName );
-    	pluginConfigModifier.replaceFileContent( pluginConfigJSONArrayString );
-    	try {
-			response.getWriter().write( this.projectName );
+
+	public void doSetProjectPluginConfig(StaplerRequest request, StaplerResponse response) {
+		//get plugin config page string
+		String pluginConfigJSONArrayString = request.getParameter("pluginConfigJSONArrayString");
+		PluginConfigManager pluginConfigModifier = new PluginConfigManager(this.mProjectName);
+		pluginConfigModifier.replaceFileContent(pluginConfigJSONArrayString);
+		try {
+			response.getWriter().write(this.mProjectName);
 			response.getWriter().close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    	
-    }
-    
-    public void doGetProjectPluginConfig( StaplerRequest request, StaplerResponse response ){
-    	PluginConfigManager pluginConfigModifier = new PluginConfigManager( this.projectName );
-    	if( !pluginConfigModifier.readFileContent().equals( "" ) ){
-	    	JsonParser parser = new JsonParser();
-	    	JsonArray pluginJsonArray = parser.parse( pluginConfigModifier.readFileContent() ).getAsJsonArray();
-	    	Gson gson = new Gson();
-	    	StringBuilder sb = new StringBuilder();
-	    	sb.append( "{success: true, data: { pluginConfigArray: [" );
-	    	for( int i = 0 ; i < pluginJsonArray.size() ; i++ ){
-	    		PluginConfig pluginConfig = gson.fromJson(pluginJsonArray.get( i ), PluginConfig.class);
-	    		if( i != 0 ){
-	    			sb.append(",");
-	    		}
-	    		sb.append( gson.toJson( pluginConfig ));
-	    	}
-	    	sb.append( "]} }" );
-	    	try {
-				response.getWriter().write( sb.toString() );
+
+	}
+
+	public void doGetProjectPluginConfig(StaplerRequest request, StaplerResponse response) {
+		PluginConfigManager pluginConfigModifier = new PluginConfigManager(this.mProjectName);
+		if (!pluginConfigModifier.readFileContent().equals("")) {
+			JsonParser parser = new JsonParser();
+			JsonArray pluginJsonArray = parser.parse(pluginConfigModifier.readFileContent()).getAsJsonArray();
+			Gson gson = new Gson();
+			StringBuilder sb = new StringBuilder();
+			sb.append("{success: true, data: { pluginConfigArray: [");
+			for (int i = 0; i < pluginJsonArray.size(); i++) {
+				PluginConfig pluginConfig = gson.fromJson(pluginJsonArray.get(i), PluginConfig.class);
+				if (i != 0) {
+					sb.append(",");
+				}
+				sb.append(gson.toJson(pluginConfig));
+			}
+			sb.append("]} }");
+			try {
+				response.getWriter().write(sb.toString());
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-    	}
-    }
-    
-    public Object getDynamic( String token, StaplerRequest request, StaplerResponse response  ){
-    	HttpSession session = request.getSession();
-    	
-    	IProject project = (IProject) SessionManager.getProject(request);
-    	if( session == null || project == null ){
-    		return this;
-    	}
-    	
-    	String projectName = project.getProjectDesc().getName();
-    	
-    	if( token.equals("productBacklog" )){ //when user enter to project
-    		return new ProductBacklog( projectName );
-    	}else if( token.equals("taskBoard" ) ){
-    		return new TaskBoard( projectName );
-    	}
-    
-    	return this;
-    }
+		}
+	}
+
+	public Object getDynamic(String token, StaplerRequest request, StaplerResponse response) {
+		HttpSession session = request.getSession();
+
+		IProject project = (IProject) SessionManager.getProject(request);
+		if (session == null || project == null) {
+			return this;
+		}
+
+		String projectName = project.getProjectDesc().getName();
+
+		if (token.equals("productBacklog")) { //when user enter to project
+			return new ProductBacklog(projectName);
+		} else if (token.equals("taskBoard")) {
+			return new TaskBoard(projectName);
+		}
+
+		return this;
+	}
+
+	public String getProjectLeftTreePluginString() {
+		// get plugin info from pluginConfig.conf in project folder
+		PluginConfigManager pluginConfigManager = new PluginConfigManager(mProjectName);
+
+		StringBuilder pluginStringList = new StringBuilder();
+		for (EzScrumUI ezScrumUI : EzScrumRoot.getLastEzScrumUIList()) {
+			if (ezScrumUI instanceof ProjectUI) {
+				PluginConfig pluginConfig = pluginConfigManager.getAvailablePluginConfigByPluginId(ezScrumUI.getPluginUI().getPluginID());
+				if (pluginConfig != null) {
+					List<String> pluginIDList = ((ProjectUI) ezScrumUI).getProjectLeftTreeIDList();
+					for (String pluginID : pluginIDList) {
+						if (pluginStringList.toString().isEmpty()) {
+							pluginStringList.append("'").append(pluginID).append("'");
+						} else {
+							pluginStringList.append(",'").append(pluginID).append("'");
+						}
+					}
+				}
+			}
+		}
+		return pluginStringList.toString();
+	}
+	
+	public String getProjectPagePluginString() {
+		// get plugin info from pluginConfig.conf in project folder
+		PluginConfigManager pluginConfigManager = new PluginConfigManager(mProjectName);
+
+		StringBuilder pluginStringList = new StringBuilder();
+		for (EzScrumUI ezScrumUI : EzScrumRoot.getLastEzScrumUIList()) {
+			if (ezScrumUI instanceof ProjectUI) {
+				PluginConfig pluginConfig = pluginConfigManager.getAvailablePluginConfigByPluginId(ezScrumUI.getPluginUI().getPluginID());
+				if (pluginConfig != null) {
+					List<String> pluginIDList = ((ProjectUI) ezScrumUI).getProjectPageIDList();
+					for (String pluginID : pluginIDList) {
+						if (pluginStringList.toString().isEmpty()) {
+							pluginStringList.append("'").append(pluginID).append("'");
+						} else {
+							pluginStringList.append(",'").append(pluginID).append("'");
+						}
+					}
+				}
+			}
+		}
+		return pluginStringList.toString();
+	}
 }
