fbb82d4ef03502f89274b19a0602f9ab6b6a1eab
samhuang <imidya@gmail.com>
2014-05-13 09:50:25 +0800

    新增連結至新版productbacklog button

--- WebContent/javascript/ezScrumPanel/ProductBacklogPanel.js
+++ WebContent/javascript/ezScrumPanel/ProductBacklogPanel.js
@@ -327,6 +327,13 @@ ezScrum.ProductBacklogPage = Ext.extend(Ext.Panel, {
 						}
 					}]
 				}
+			}, {
+				itemId: 'ProductBacklog_goToNewPage',
+				text: 'New ProductBacklog V2',
+				icon: 'images/arrow_right.png',
+				handler: function() {
+					window.location.assign("/ezScrum/showProductBacklogV2.do?PID=" + getURLParameter('PID'));
+				}
 			}]
 		});
 
