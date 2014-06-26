0a4e4ccb5794150c396169d2f5a6606c6a38ef40
samhuang <imidya@gmail.com>
2014-06-26 11:30:41 +0800

    modify sql file format

--- WebContent/Workspace/_metadata/initial_bk.sql
+++ WebContent/Workspace/_metadata/initial_bk.sql
@@ -1,27 +1,3 @@
--- MySQL dump 10.11
---
--- Host: localhost    Database: bugtracker
--- ------------------------------------------------------
--- Server version	5.0.45-community-nt-log
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FORE_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `buildresult`
---
-/*ghghghghghgh*/
-
-
-
 DROP TABLE IF EXISTS `buildresult`;
 CREATE TABLE `buildresult` (
   `ID` int(11) NOT NULL auto_increment,
@@ -1040,8 +1016,6 @@ CREATE TABLE `mantis_user_print_pref_table` (
 --
 
 LOCK TABLES `mantis_user_print_pref_table` WRITE;
-/*!40000 ALTER TABLE `mantis_user_print_pref_table` DISABLE KEYS */;
-/*!40000 ALTER TABLE `mantis_user_print_pref_table` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -1129,18 +1103,6 @@ LOCK TABLES `query` WRITE;
 /*!40000 ALTER TABLE `query` DISABLE KEYS */;
 /*!40000 ALTER TABLE `query` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2010-07-16  1:47:31
-
 
 /**
  * the new ezScrum 1.8 table
