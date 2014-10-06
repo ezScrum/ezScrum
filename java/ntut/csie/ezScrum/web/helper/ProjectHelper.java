082b7b5c230a25daef078abce916531120bc12ba
AllenHuang <a26732812@gmail.com>
2014-10-06 15:10:41 +0800

    finish dounload access power

--- java/ntut/csie/ezScrum/web/helper/ProjectHelper.java
+++ java/ntut/csie/ezScrum/web/helper/ProjectHelper.java
@@ -2,6 +2,7 @@ package ntut.csie.ezScrum.web.helper;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
