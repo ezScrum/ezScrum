0db673465200ff94d339404bd267b4c7d5c60973
AllenHuang <a26732812@gmail.com>
2014-11-24 17:29:25 +0800

    DAO 初體驗!?

--- java/ntut/csie/ezScrum/DAO/AbstractDAO.java
+++ java/ntut/csie/ezScrum/DAO/AbstractDAO.java
@@ -1,5 +1,26 @@
 package ntut.csie.ezScrum.DAO;
 
-public class AbstractDAO {
+import ntut.csie.ezScrum.issue.sql.service.core.Configuration;
+import ntut.csie.ezScrum.issue.sql.service.tool.internal.MySQLControl;
 
+public abstract class AbstractDAO<T, K> {
+	protected Configuration mConfig;
+	protected static Object sInstance = null;
+	protected MySQLControl mControl;
+	
+	protected AbstractDAO() {
+		mConfig = new Configuration();
+		mControl = new MySQLControl(mConfig.getServerUrl(), "3306", mConfig.getDBAccount());
+		mControl.setUser(mConfig.getDBAccount());
+		mControl.setPassword(mConfig.getDBPassword());
+	}
+	
+	// create
+	abstract public long add(K infoObject);
+	// read
+	abstract public T get(long id);
+	// update
+	abstract public boolean edit(T dataObject);
+	// delete
+	abstract public boolean delete(long id);
 }
