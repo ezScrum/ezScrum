836444c96c774b085268d798432150cefc5febbd
AllenHuang <a26732812@gmail.com>
2014-12-18 14:58:02 +0800

    modify TaskObject

--- java/ntut/csie/ezScrum/web/dataObject/TaskObject.java
+++ java/ntut/csie/ezScrum/web/dataObject/TaskObject.java
@@ -1,45 +1,408 @@
 package ntut.csie.ezScrum.web.dataObject;
 
-import ntut.csie.ezScrum.issue.core.IIssue;
+import java.util.ArrayList;
+import java.util.Date;
+
+import ntut.csie.ezScrum.web.databasEnum.TaskEnum;
+
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
 
 public class TaskObject {
-	public String id = "";
-	public String name = "";
-	public String estimation = "";
-	public String status = "";
-	public String handler = "";
-	public String partners = "";
-	public String remains = "";
-	public String actual = "";
-	public String notes = "";
-	public String specificTime = "";
-	public long doneTime;
+	public final static int STATUS_UNCHECK = 1;
+	public final static int STATUS_CHECK = 2;
+	public final static int STATUS_DONE = 3;
+	public final static int WILD = -1;
 	
-	public TaskObject() {}
+	private long mId = -1;
+	private long mSerialId = -1;
+	private long mProjectId = -1;
+	private long mStoryId = -1;
+	private long mHandlerId = -1;
+	private String mName = "";
+	private String mNotes = "";
+	private int mEstimate = 0;
+	private int mRemains = 0;
+	private int mActual = 0;
+	private int mStatus = -1;
+	private ArrayList<Long> mPartnersId = new ArrayList<Long>();
+	private ArrayList<AttachFileObject> mAttachFiles = new ArrayList<AttachFileObject>();
+	private ArrayList<HistoryObject> mHistories = new ArrayList<HistoryObject>();
+	private long mCreateTime = -1;
+	private long mUpdateTime = -1;
 	
-	public TaskObject(IIssue task) {
-		id = Long.toString(task.getIssueID());
-		name = task.getSummary();
-		estimation = task.getEstimated();
-		status = task.getStatus();
-		handler = task.getAssignto();
-		partners = task.getPartners();
-		remains = task.getRemains();
-		actual = task.getActualHour();
-		notes = task.getNotes();
+	private TaskObject(TaskObject.Builder builder) {
+		this.mId = builder.mId;
+		this.mSerialId = builder.mSerialId;
+		this.mProjectId = builder.mProjectId;
+		this.mStoryId = builder.mStoryId;
+		this.mHandlerId = builder.mHandlerId;
+		this.mName = builder.mName;
+		this.mNotes = builder.mNotes;
+		this.mEstimate = builder.mEstimate;
+		this.mRemains = builder.mRemains;
+		this.mActual = builder.mActual;
+		this.mStatus = builder.mStatus;
+		this.mPartnersId = builder.mPartnersId;
+		this.mAttachFiles = builder.mAttachFiles;
+		this.mHistories = builder.mHistories;
+		this.mCreateTime = builder.mCreateTime;
+		this.mUpdateTime = builder.mUpdateTime;
+	}
+
+	public String toString() {
+		try {
+			return toJSON().toString();
+		} catch (JSONException e) {
+			return "JSON Exception";
+		}
+	}
+
+	public void setId(long id) {
+		this.mId = id;
+	}
+
+	public void setSerialId(long serialId) {
+		this.mSerialId = serialId;
+	}
+
+	public void setName(String name) {
+		this.mName = name;
+	}
+
+	public void setHandlerId(long handlerId) {
+		this.mHandlerId = handlerId;
+	}
+
+	public void setEstimate(int estimate) {
+		this.mEstimate = estimate;
+	}
+
+	public void setRemains(int remains) {
+		this.mRemains = remains;
+	}
+
+	public void setActual(int actual) {
+		this.mActual = actual;
+	}
+
+	public void setNotes(String notes) {
+		this.mNotes = notes;
+	}
+
+	public void setStatus(int status) {
+		this.mStatus = status;
+	}
+
+	public void setProjectId(long projectId) {
+		this.mProjectId = projectId;
+	}
+
+	public void setStoryId(long storyId) {
+		this.mStoryId = storyId;
+	}
+
+	public void setCreateTime(long createtime) {
+		this.mCreateTime = createtime;
+	}
+
+	public void setUpdateTime(long updatetime) {
+		this.mUpdateTime = updatetime;
+	}
+
+	public void setPartnersId(ArrayList<Long> partnersId) {
+		this.mPartnersId = partnersId;
 	}
 	
-	public String toString() {
-		String task = 
-				"id :" + id + 
-				", name :" + name +
-				", estimation :" + estimation +
-				", status :" + status +
-				", handler :" + handler +
-				", partners :" + partners +
-				", remains :" + remains +
-				", actual :" + actual +
-				", notes :" + notes;
+	public void setAttachFiles(ArrayList<AttachFileObject> attachFiles) {
+		this.mAttachFiles = attachFiles;
+	}
+
+	public void setHistories(ArrayList<HistoryObject> histories) {
+		this.mHistories = histories;
+	}
+
+	public void addPartner(long partnerId) {
+		this.mPartnersId.add(partnerId);
+	}
+	
+	public void addAttachFile(AttachFileObject attachFile) {
+		this.mAttachFiles.add(attachFile);
+	}
+
+	public void addHistory(HistoryObject history) {
+		this.mHistories.add(history);
+	}
+
+	public void removePartner(long partnerId) {
+		this.mPartnersId.remove(partnerId);
+	}
+
+	public long getId() {
+		return mId;
+	}
+
+	public long getSerialId() {
+		return mSerialId;
+	}
+
+	public String getName() {
+		return mName;
+	}
+
+	public long getHandlerId() {
+		return mHandlerId;
+	}
+
+	public int getEstimate() {
+		return mEstimate;
+	}
+
+	public int getRemains() {
+		return mRemains;
+	}
+
+	public int getActual() {
+		return mActual;
+	}
+
+	public String getNotes() {
+		return mNotes;
+	}
+
+	public int getStatus() {
+		return mStatus;
+	}
+	
+	public int getStatus(Date date) {
+		long time = date.getTime();
+		time += 1000 * 60 * 60 * 24 - 1;
+		date = new Date(time);
+		
+		int status = TaskObject.STATUS_UNCHECK;
+		for (HistoryObject history : mHistories) {
+			if (history.getHistoryType() == HistoryObject.TYPE_STATUS
+					&& (new Date(history.getModifiedTime()).before(date) )) {
+				if (history.getNewValue().equals("\"Checked Out\"")) {
+					status = TaskObject.STATUS_CHECK;
+				} else if (history.getNewValue().equals("\"Done\"")) {
+					status = TaskObject.STATUS_DONE;
+				}
+			}
+		}
+		return status;
+	}
+	
+	public int getRemainsByDate(Date date) {
+		return searchValue(HistoryObject.TYPE_REMAIMS, date);
+	}
+	
+	public int getEstimateByDate(Date date) {
+		return searchValue(HistoryObject.TYPE_ESTIMATE, date);
+	}
+	
+	private int searchValue(int searchType, Date date) {
+		int value = -1;
+		for (HistoryObject history : mHistories) {
+			if (history.getHistoryType() == searchType
+					&& (new Date(history.getModifiedTime()).before(date))) {
+				value = Integer.parseInt(history.getNewValue());
+			}
+		}
+		return value;
+	}
+
+	public long getProjectId() {
+		return mProjectId;
+	}
+
+	public long getStoryId() {
+		return mStoryId;
+	}
+
+	public long getCreateTime() {
+		return mCreateTime;
+	}
+
+	public long getUpdateTime() {
+		return mUpdateTime;
+	}
+
+	public ArrayList<Long> getPartnersId() {
+		return mPartnersId;
+	}
+
+	public ArrayList<HistoryObject> getHistories() {
+		return mHistories;
+	}
+	
+	public ArrayList<AttachFileObject> getAttachFiles() {
+		return mAttachFiles;
+	}
+	
+	public int getDateStatus(Date date) {
+		int status = -1;
+		for (HistoryObject history : mHistories) {
+			if (history.getHistoryType() == HistoryObject.TYPE_STATUS) {
+				history.getOldValue();
+			}
+		}
+		return status;
+	}
+	
+	public long getAssignedTime() {
+		long assignedTime = 0;
+		for (HistoryObject history : mHistories) {
+			if (history.getHistoryType() == HistoryObject.TYPE_STATUS
+					&& history.getNewValue().equals("\"Checked Out\"")) {
+				if (assignedTime < history.getModifiedTime()) {
+					assignedTime = history.getModifiedTime();
+				}
+			}
+		}
+		return assignedTime;
+	}
+	
+	public long getDoneTime() {
+		long doneTime = 0;
+		for (HistoryObject history : mHistories) {
+			if (history.getHistoryType() == HistoryObject.TYPE_STATUS 
+					&& history.getNewValue().equals("\"Done\"")) {
+				if (doneTime < history.getModifiedTime()) {
+					doneTime = history.getModifiedTime();
+				}
+			}
+		}
+		return doneTime;
+	}
+	
+	public JSONObject toJSON() throws JSONException {
+		JSONObject task = new JSONObject();
+		JSONArray partners = new JSONArray();
+
+		for(Long partnerId : mPartnersId) {
+			partners.put(partnerId);
+		}
+		
+		task
+			.put(TaskEnum.NAME, mName)
+			.put(TaskEnum.ESTIMATE, mEstimate)
+			.put(TaskEnum.ACTUAL, mActual)
+			.put(TaskEnum.STORY_ID, mStoryId)
+			.put(TaskEnum.PROJECT_ID, mProjectId)
+			.put(TaskEnum.NOTES, mNotes)
+			.put(TaskEnum.REMAIN, mRemains)
+			.put(TaskEnum.STATUS, mStatus)
+			.put(TaskEnum.SERIAL_ID, mSerialId)
+			.put(TaskEnum.ID, mId)
+			.put(TaskEnum.CREATE_TIME, mCreateTime)
+			.put(TaskEnum.UPDATE_TIME, mUpdateTime)
+			.put("partners", partners);
+		
 		return task;
+	}
+
+	public static class Builder {
+		private long mId = -1;
+		private long mSerialId = -1;
+		private long mProjectId = -1;
+		private long mStoryId = -1;
+		private long mHandlerId = -1;
+		private String mName = "";
+		private String mNotes = "";
+		private int mEstimate = 0;
+		private int mRemains = 0;
+		private int mActual = 0;
+		private int mStatus = -1;
+		private ArrayList<Long> mPartnersId = new ArrayList<Long>();
+		private ArrayList<AttachFileObject> mAttachFiles = new ArrayList<AttachFileObject>();
+		private ArrayList<HistoryObject> mHistories = new ArrayList<HistoryObject>();
+		private long mCreateTime;
+		private long mUpdateTime;
+		
+		public TaskObject build() {
+			return new TaskObject(this);
+		}
+
+		public Builder setId(long id) {
+			this.mId = id;
+			return this;
+		}
+
+		public Builder setSerialId(long serialId) {
+			this.mSerialId = serialId;
+			return this;
+		}
+
+		public Builder setProjectId(long projectId) {
+			this.mProjectId = projectId;
+			return this;
+		}
+
+		public Builder setStoryId(long storyId) {
+			this.mStoryId = storyId;
+			return this;
+		}
+
+		public Builder setHandlerId(long handlerId) {
+			this.mHandlerId = handlerId;
+			return this;
+		}
+
+		public Builder setName(String name) {
+			this.mName = name;
+			return this;
+		}
+
+		public Builder setNotes(String notes) {
+			this.mNotes = notes;
+			return this;
+		}
+
+		public Builder setEstimate(int estimate) {
+			this.mEstimate = estimate;
+			return this;
+		}
+
+		public Builder setRemains(int remains) {
+			this.mRemains = remains;
+			return this;
+		}
+
+		public Builder setActual(int actual) {
+			this.mActual = actual;
+			return this;
+		}
+
+		public Builder setStatus(int status) {
+			this.mStatus = status;
+			return this;
+		}
+
+		public Builder setPartnersId(ArrayList<Long> partnersId) {
+			this.mPartnersId = partnersId;
+			return this;
+		}
+		
+		public Builder setAttachFiles(ArrayList<AttachFileObject> attachFiles) {
+			this.mAttachFiles = attachFiles;
+			return this;
+		}
+
+		public Builder setHistories(ArrayList<HistoryObject> histories) {
+			this.mHistories = histories;
+			return this;
+		}
+		
+		public Builder setCreateTime(long createTime) {
+			this.mCreateTime = createTime;
+			return this;
+		}
+		
+		public Builder setUpdateTime(long updateTime) {
+			this.mUpdateTime = updateTime;
+			return this;
+		}
 	}
 }
