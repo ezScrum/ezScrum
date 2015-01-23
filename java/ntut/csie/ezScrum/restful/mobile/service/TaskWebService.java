1be9842bf448bd79786334c1871a83fd9284abc8
samhuang <imidya@gmail.com>
2015-01-23 10:25:13 +0800

    add several test cases

--- java/ntut/csie/ezScrum/restful/mobile/service/TaskWebService.java
+++ java/ntut/csie/ezScrum/restful/mobile/service/TaskWebService.java
@@ -55,7 +55,7 @@ public class TaskWebService extends ProjectWebService {
 	}
 	
 	public String getWildTask() throws NumberFormatException, SQLException {
-		return mSprintBacklogHelper.getWildTasks(mProject.getId());
+		return mSprintBacklogHelper.getTasksWithNoParent(mProject.getId());
 	}
 	
 	/**
