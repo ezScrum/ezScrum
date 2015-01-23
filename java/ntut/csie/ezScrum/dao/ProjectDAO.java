781925e4e30ebbb5bec722bea99ae6a00fb54595
AllenHuang <a26732812@gmail.com>
2015-01-23 11:41:30 +0800

    finish edit ProjectDAOTest

--- java/ntut/csie/ezScrum/dao/ProjectDAO.java
+++ java/ntut/csie/ezScrum/dao/ProjectDAO.java
@@ -22,11 +22,11 @@ public class ProjectDAO extends AbstractDAO<ProjectObject, ProjectObject> {
 		}
 		return sInstance;
 	}
-	
+
 	@Override
-    public long create(ProjectObject project) {
+	public long create(ProjectObject project) {
 		long createTime = System.currentTimeMillis();
-		
+
 		IQueryValueSet valueSet = new MySQLQuerySet();
 		valueSet.addTableName(ProjectEnum.TABLE_NAME);
 		valueSet.addInsertValue(ProjectEnum.NAME, project.getName());
@@ -38,18 +38,18 @@ public class ProjectDAO extends AbstractDAO<ProjectObject, ProjectObject> {
 		valueSet.addInsertValue(ProjectEnum.UPDATE_TIME, createTime);
 		String query = valueSet.getInsertQuery();
 		long id = mControl.executeInsert(query);
-		
+
 		// create default role
 		ScrumRole scrumRole;
 		for (RoleEnum role : RoleEnum.values()) {
 			scrumRole = new ScrumRole(role);
 			createScrumRole(id, role, scrumRole);
 		}
-		
+
 		return id;
-    }
-	
-	private long createScrumRole(long projectId, RoleEnum role, ScrumRole scrumRole) {
+	}
+
+	public long createScrumRole(long projectId, RoleEnum role, ScrumRole scrumRole) {
 		IQueryValueSet valueSet = new MySQLQuerySet();
 		valueSet.addTableName(ScrumRoleEnum.TABLE_NAME);
 		valueSet.addInsertValue(ScrumRoleEnum.PROJECT_ID, projectId);
@@ -68,18 +68,18 @@ public class ProjectDAO extends AbstractDAO<ProjectObject, ProjectObject> {
 		String query = valueSet.getInsertQuery();
 		return mControl.executeInsert(query);
 	}
-	
+
 	public ScrumRole getScrumRole(long id, String projectName, RoleEnum role) {
 		MySQLQuerySet valueSet = new MySQLQuerySet();
 		valueSet.addTableName(ScrumRoleEnum.TABLE_NAME);
 		valueSet.addEqualCondition(ScrumRoleEnum.PROJECT_ID, id);
 		valueSet.addEqualCondition(ScrumRoleEnum.ROLE, String.valueOf(role.ordinal()));
 		String query = valueSet.getSelectQuery();
-		
+
 		ResultSet result = mControl.executeQuery(query);
 		try {
 			if (result.first()) {
-				return getScrumRole(projectName, role.name(), result);
+				return convertScrumRole(projectName, role.name(), result);
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -88,28 +88,28 @@ public class ProjectDAO extends AbstractDAO<ProjectObject, ProjectObject> {
 		}
 		return null;
 	}
-	
-	private ScrumRole getScrumRole(String projectName, String role, ResultSet result) {
+
+	private ScrumRole convertScrumRole(String projectName, String role, ResultSet result) {
 		ScrumRole scrumRole = new ScrumRole(projectName, role);
 		scrumRole.setisGuest(RoleEnum.Guest == RoleEnum.valueOf(role));
 		try {
-	        scrumRole.setAccessProductBacklog(result.getBoolean(ScrumRoleEnum.ACCESS_PRODUCT_BACKLOG));
-	        scrumRole.setAccessReleasePlan(result.getBoolean(ScrumRoleEnum.ACCESS_RELEASE_PLAN));
-	        scrumRole.setReadReport(result.getBoolean(ScrumRoleEnum.ACCESS_REPORT));
-	        scrumRole.setAccessRetrospective(result.getBoolean(ScrumRoleEnum.ACCESS_RETROSPECTIVE));
-	        scrumRole.setAccessSprintBacklog(result.getBoolean(ScrumRoleEnum.ACCESS_SPRINT_BACKLOG));
-	        scrumRole.setAccessSprintPlan(result.getBoolean(ScrumRoleEnum.ACCESS_SPRINT_PLAN));
-	        scrumRole.setAccessUnplannedItem(result.getBoolean(ScrumRoleEnum.ACCESS_UNPLANNED));
-	        scrumRole.setAccessTaskBoard(result.getBoolean(ScrumRoleEnum.ACCESS_TASKBOARD));
-	        scrumRole.setEditProject(result.getBoolean(ScrumRoleEnum.ACCESS_EDIT_PROJECT));
-        } catch (SQLException e) {
-	        e.printStackTrace();
-        } finally {
-        	closeResultSet(result);
-        }
+			scrumRole.setAccessProductBacklog(result.getBoolean(ScrumRoleEnum.ACCESS_PRODUCT_BACKLOG));
+			scrumRole.setAccessReleasePlan(result.getBoolean(ScrumRoleEnum.ACCESS_RELEASE_PLAN));
+			scrumRole.setReadReport(result.getBoolean(ScrumRoleEnum.ACCESS_REPORT));
+			scrumRole.setAccessRetrospective(result.getBoolean(ScrumRoleEnum.ACCESS_RETROSPECTIVE));
+			scrumRole.setAccessSprintBacklog(result.getBoolean(ScrumRoleEnum.ACCESS_SPRINT_BACKLOG));
+			scrumRole.setAccessSprintPlan(result.getBoolean(ScrumRoleEnum.ACCESS_SPRINT_PLAN));
+			scrumRole.setAccessUnplannedItem(result.getBoolean(ScrumRoleEnum.ACCESS_UNPLANNED));
+			scrumRole.setAccessTaskBoard(result.getBoolean(ScrumRoleEnum.ACCESS_TASKBOARD));
+			scrumRole.setEditProject(result.getBoolean(ScrumRoleEnum.ACCESS_EDIT_PROJECT));
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			closeResultSet(result);
+		}
 		return scrumRole;
 	}
-	
+
 	public boolean updateScrumRole(long projectId, RoleEnum role, ScrumRole scrumRole) {
 		IQueryValueSet valueSet = new MySQLQuerySet();
 		valueSet.addTableName(ScrumRoleEnum.TABLE_NAME);
@@ -130,12 +130,12 @@ public class ProjectDAO extends AbstractDAO<ProjectObject, ProjectObject> {
 	}
 
 	@Override
-    public ProjectObject get(long id) {
+	public ProjectObject get(long id) {
 		IQueryValueSet valueSet = new MySQLQuerySet();
 		valueSet.addTableName(ProjectEnum.TABLE_NAME);
 		valueSet.addEqualCondition(ProjectEnum.ID, id);
 		String query = valueSet.getSelectQuery();
-		
+
 		ResultSet result = mControl.executeQuery(query);
 
 		ProjectObject project = null;
@@ -149,12 +149,12 @@ public class ProjectDAO extends AbstractDAO<ProjectObject, ProjectObject> {
 			closeResultSet(result);
 		}
 		return project;
-    }
+	}
 
 	@Override
-    public boolean update(ProjectObject project) {
+	public boolean update(ProjectObject project) {
 		long currentTime = System.currentTimeMillis();
-		
+
 		IQueryValueSet valueSet = new MySQLQuerySet();
 		valueSet.addTableName(ProjectEnum.TABLE_NAME);
 		valueSet.addInsertValue(ProjectEnum.DISPLAY_NAME, project.getDisplayName());
@@ -164,25 +164,25 @@ public class ProjectDAO extends AbstractDAO<ProjectObject, ProjectObject> {
 		valueSet.addInsertValue(ProjectEnum.UPDATE_TIME, currentTime);
 		valueSet.addEqualCondition(ProjectEnum.ID, project.getId());
 		String query = valueSet.getUpdateQuery();
-		
+
 		return mControl.executeUpdate(query);
-    }
+	}
 
 	@Override
-    public boolean delete(long id) {
+	public boolean delete(long id) {
 		IQueryValueSet valueSet = new MySQLQuerySet();
 		valueSet.addTableName(ProjectEnum.TABLE_NAME);
 		valueSet.addEqualCondition(ProjectEnum.ID, id);
 		String query = valueSet.getDeleteQuery();
 		return mControl.executeUpdate(query);
-    }
-	
+	}
+
 	public ProjectObject getProjectByName(String name) {
 		IQueryValueSet valueSet = new MySQLQuerySet();
 		valueSet.addTableName(ProjectEnum.TABLE_NAME);
 		valueSet.addTextFieldEqualCondition(ProjectEnum.NAME, name);
 		String query = valueSet.getSelectQuery();
-		
+
 		ResultSet result = mControl.executeQuery(query);
 
 		ProjectObject project = null;
@@ -197,44 +197,37 @@ public class ProjectDAO extends AbstractDAO<ProjectObject, ProjectObject> {
 		}
 		return project;
 	}
-	
+
 	public ArrayList<ProjectObject> getProjects() {
 		IQueryValueSet valueSet = new MySQLQuerySet();
 		valueSet.addTableName(ProjectEnum.TABLE_NAME);
 		String query = valueSet.getSelectQuery();
-		
+
 		ResultSet result = mControl.executeQuery(query);
-		
+
 		ArrayList<ProjectObject> projects = new ArrayList<ProjectObject>();
 		try {
-	        while (result.next()) {
-	        	projects.add(convert(result));
-	        }
-        } catch (SQLException e) {
-	        e.printStackTrace();
-        } finally {
-        	closeResultSet(result);
-        }
+			while (result.next()) {
+				projects.add(convert(result));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			 closeResultSet(result);
+		}
 		return projects;
 	}
-	
-	private ProjectObject convert(ResultSet result) {
-		ProjectObject project = null;
-        try {
-	        project = new ProjectObject(result.getLong(ProjectEnum.ID),
-	        		result.getString(ProjectEnum.NAME));
-	        project
-	        .setDisplayName(result.getString(ProjectEnum.DISPLAY_NAME))
-	        .setComment(result.getString(ProjectEnum.COMMENT))
-	        .setManager(result.getString(ProjectEnum.PRODUCT_OWNER))
-	        .setAttachFileSize(result.getLong(ProjectEnum.ATTATCH_MAX_SIZE))
-	        .setCreateTime(result.getLong(ProjectEnum.CREATE_TIME))
-	        .setUpdateTime(result.getLong(ProjectEnum.UPDATE_TIME));
-        } catch (SQLException e) {
-	        e.printStackTrace();
-        } finally {
-        	closeResultSet(result);
-        }
+
+	private ProjectObject convert(ResultSet result) throws SQLException {
+		ProjectObject project = new ProjectObject(result.getLong(ProjectEnum.ID),
+		        result.getString(ProjectEnum.NAME));
+		project
+		        .setDisplayName(result.getString(ProjectEnum.DISPLAY_NAME))
+		        .setComment(result.getString(ProjectEnum.COMMENT))
+		        .setManager(result.getString(ProjectEnum.PRODUCT_OWNER))
+		        .setAttachFileSize(result.getLong(ProjectEnum.ATTATCH_MAX_SIZE))
+		        .setCreateTime(result.getLong(ProjectEnum.CREATE_TIME))
+		        .setUpdateTime(result.getLong(ProjectEnum.UPDATE_TIME));
 		return project;
 	}
 }
--- test/ntut/csie/ezScrum/dao/ProjectDAOTest.java
+++ test/ntut/csie/ezScrum/dao/ProjectDAOTest.java
@@ -4,10 +4,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-
 import junit.framework.TestCase;
 import ntut.csie.ezScrum.issue.sql.service.core.Configuration;
 import ntut.csie.ezScrum.issue.sql.service.core.IQueryValueSet;
@@ -20,6 +16,10 @@ import ntut.csie.ezScrum.web.dataObject.ProjectObject;
 import ntut.csie.ezScrum.web.databasEnum.ProjectEnum;
 import ntut.csie.ezScrum.web.databasEnum.RoleEnum;
 import ntut.csie.ezScrum.web.databasEnum.ScrumRoleEnum;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 public class ProjectDAOTest extends TestCase {
 	private MySQLControl mControl = null;
@@ -82,17 +82,17 @@ public class ProjectDAOTest extends TestCase {
 		IQueryValueSet valueSet = new MySQLQuerySet();
 		valueSet.addTableName(ProjectEnum.TABLE_NAME);
 		String query = valueSet.getSelectQuery();
-		
+
 		ResultSet result = mControl.executeQuery(query);
 		try {
-	        while (result.next()) {
-	        	projects.add(convertProject(result));
-	        }
-        } catch (SQLException e) {
-	        e.printStackTrace();
-        } finally {
-        	closeResultSet(result);
-        }
+			while (result.next()) {
+				projects.add(convertProject(result));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			closeResultSet(result);
+		}
 
 		assertEquals(3, projects.size());
 		for (int i = 0; i < 3; i++) {
@@ -106,9 +106,9 @@ public class ProjectDAOTest extends TestCase {
 			assertNotNull(projects.get(i).getUpdateTime());
 		}
 	}
-	
+
 	@Test
-	public void testCreateWithScrumRole() {
+	public void testCreateScrumRoleInCreate() {
 		// create test data
 		ProjectObject project = new ProjectObject("TEST_PROJECT_1");
 		project
@@ -125,17 +125,197 @@ public class ProjectDAOTest extends TestCase {
 		valueSet.addTableName(ScrumRoleEnum.TABLE_NAME);
 		valueSet.addEqualCondition(ScrumRoleEnum.PROJECT_ID, projectId);
 		String query = valueSet.getSelectQuery();
-		
+
 		ResultSet result = mControl.executeQuery(query);
+		ArrayList<ScrumRole> scrumRoles = new ArrayList<ScrumRole>();
 		try {
-			if (result.first()) {
-				return convertScrumRole(project.getName(), role.name(), result);
+			for (RoleEnum role : RoleEnum.values()) {
+				if (result.next()) {
+					scrumRoles.add(convertScrumRole(project.getName(), role.name(), result));
+				}
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
 			closeResultSet(result);
 		}
+
+		assertEquals(5, scrumRoles.size());
+		// assert ProductOwner role
+		assertTrue(scrumRoles.get(0).getAccessProductBacklog());
+		assertTrue(scrumRoles.get(0).getAccessReleasePlan());
+		assertTrue(scrumRoles.get(0).getAccessRetrospective());
+		assertTrue(scrumRoles.get(0).getAccessSprintBacklog());
+		assertTrue(scrumRoles.get(0).getAccessSprintPlan());
+		assertTrue(scrumRoles.get(0).getAccessTaskBoard());
+		assertTrue(scrumRoles.get(0).getAccessUnplannedItem());
+		assertTrue(scrumRoles.get(0).getEditProject());
+		assertTrue(scrumRoles.get(0).getReadReport());
+		// assert ScrumMaster role
+		assertTrue(scrumRoles.get(1).getAccessProductBacklog());
+		assertTrue(scrumRoles.get(1).getAccessReleasePlan());
+		assertTrue(scrumRoles.get(1).getAccessRetrospective());
+		assertTrue(scrumRoles.get(1).getAccessSprintBacklog());
+		assertTrue(scrumRoles.get(1).getAccessSprintPlan());
+		assertTrue(scrumRoles.get(1).getAccessTaskBoard());
+		assertTrue(scrumRoles.get(1).getAccessUnplannedItem());
+		assertTrue(scrumRoles.get(1).getEditProject());
+		assertTrue(scrumRoles.get(1).getReadReport());
+		// assert ScrumTeam role
+		assertTrue(scrumRoles.get(2).getAccessProductBacklog());
+		assertTrue(scrumRoles.get(2).getAccessReleasePlan());
+		assertTrue(scrumRoles.get(2).getAccessRetrospective());
+		assertTrue(scrumRoles.get(2).getAccessSprintBacklog());
+		assertTrue(scrumRoles.get(2).getAccessSprintPlan());
+		assertTrue(scrumRoles.get(2).getAccessTaskBoard());
+		assertTrue(scrumRoles.get(2).getAccessUnplannedItem());
+		assertFalse(scrumRoles.get(2).getEditProject());
+		assertTrue(scrumRoles.get(2).getReadReport());
+		// assert Stakeholder role
+		assertFalse(scrumRoles.get(3).getAccessProductBacklog());
+		assertFalse(scrumRoles.get(3).getAccessReleasePlan());
+		assertFalse(scrumRoles.get(3).getAccessRetrospective());
+		assertFalse(scrumRoles.get(3).getAccessSprintBacklog());
+		assertFalse(scrumRoles.get(3).getAccessSprintPlan());
+		assertFalse(scrumRoles.get(3).getAccessTaskBoard());
+		assertFalse(scrumRoles.get(3).getAccessUnplannedItem());
+		assertFalse(scrumRoles.get(3).getEditProject());
+		assertTrue(scrumRoles.get(3).getReadReport());
+		// assert Guest role
+		assertFalse(scrumRoles.get(4).getAccessProductBacklog());
+		assertFalse(scrumRoles.get(4).getAccessReleasePlan());
+		assertFalse(scrumRoles.get(4).getAccessRetrospective());
+		assertFalse(scrumRoles.get(4).getAccessSprintBacklog());
+		assertFalse(scrumRoles.get(4).getAccessSprintPlan());
+		assertFalse(scrumRoles.get(4).getAccessTaskBoard());
+		assertFalse(scrumRoles.get(4).getAccessUnplannedItem());
+		assertFalse(scrumRoles.get(4).getEditProject());
+		assertFalse(scrumRoles.get(4).getReadReport());
+	}
+
+	@Test
+	public void testGetScrumRole() {
+		// create test data
+		ProjectObject project = new ProjectObject("TEST_PROJECT_1");
+		project
+		        .setDisplayName("TEST_DISPLATNAME_1")
+		        .setComment("TEST_COMMON_1")
+		        .setManager("TEST_MANAGER")
+		        .setAttachFileSize(2);
+		long projectId = ProjectDAO.getInstance().create(project);
+		assertNotSame(-1, projectId);
+
+		// 從 DB 裡取出 project 的 Scrum role 資料
+		ArrayList<ScrumRole> scrumRoles = new ArrayList<ScrumRole>();
+		for (RoleEnum role : RoleEnum.values()) {
+			scrumRoles.add(ProjectDAO.getInstance().getScrumRole(projectId, "TEST_PROJECT_1", role));
+		}
+
+		assertEquals(5, scrumRoles.size());
+		// assert ProductOwner role
+		assertTrue(scrumRoles.get(0).getAccessProductBacklog());
+		assertTrue(scrumRoles.get(0).getAccessReleasePlan());
+		assertTrue(scrumRoles.get(0).getAccessRetrospective());
+		assertTrue(scrumRoles.get(0).getAccessSprintBacklog());
+		assertTrue(scrumRoles.get(0).getAccessSprintPlan());
+		assertTrue(scrumRoles.get(0).getAccessTaskBoard());
+		assertTrue(scrumRoles.get(0).getAccessUnplannedItem());
+		assertTrue(scrumRoles.get(0).getEditProject());
+		assertTrue(scrumRoles.get(0).getReadReport());
+		// assert ScrumMaster role
+		assertTrue(scrumRoles.get(1).getAccessProductBacklog());
+		assertTrue(scrumRoles.get(1).getAccessReleasePlan());
+		assertTrue(scrumRoles.get(1).getAccessRetrospective());
+		assertTrue(scrumRoles.get(1).getAccessSprintBacklog());
+		assertTrue(scrumRoles.get(1).getAccessSprintPlan());
+		assertTrue(scrumRoles.get(1).getAccessTaskBoard());
+		assertTrue(scrumRoles.get(1).getAccessUnplannedItem());
+		assertTrue(scrumRoles.get(1).getEditProject());
+		assertTrue(scrumRoles.get(1).getReadReport());
+		// assert ScrumTeam role
+		assertTrue(scrumRoles.get(2).getAccessProductBacklog());
+		assertTrue(scrumRoles.get(2).getAccessReleasePlan());
+		assertTrue(scrumRoles.get(2).getAccessRetrospective());
+		assertTrue(scrumRoles.get(2).getAccessSprintBacklog());
+		assertTrue(scrumRoles.get(2).getAccessSprintPlan());
+		assertTrue(scrumRoles.get(2).getAccessTaskBoard());
+		assertTrue(scrumRoles.get(2).getAccessUnplannedItem());
+		assertFalse(scrumRoles.get(2).getEditProject());
+		assertTrue(scrumRoles.get(2).getReadReport());
+		// assert Stakeholder role
+		assertFalse(scrumRoles.get(3).getAccessProductBacklog());
+		assertFalse(scrumRoles.get(3).getAccessReleasePlan());
+		assertFalse(scrumRoles.get(3).getAccessRetrospective());
+		assertFalse(scrumRoles.get(3).getAccessSprintBacklog());
+		assertFalse(scrumRoles.get(3).getAccessSprintPlan());
+		assertFalse(scrumRoles.get(3).getAccessTaskBoard());
+		assertFalse(scrumRoles.get(3).getAccessUnplannedItem());
+		assertFalse(scrumRoles.get(3).getEditProject());
+		assertTrue(scrumRoles.get(3).getReadReport());
+		// assert Guest role
+		assertFalse(scrumRoles.get(4).getAccessProductBacklog());
+		assertFalse(scrumRoles.get(4).getAccessReleasePlan());
+		assertFalse(scrumRoles.get(4).getAccessRetrospective());
+		assertFalse(scrumRoles.get(4).getAccessSprintBacklog());
+		assertFalse(scrumRoles.get(4).getAccessSprintPlan());
+		assertFalse(scrumRoles.get(4).getAccessTaskBoard());
+		assertFalse(scrumRoles.get(4).getAccessUnplannedItem());
+		assertFalse(scrumRoles.get(4).getEditProject());
+		assertFalse(scrumRoles.get(4).getReadReport());
+	}
+
+	@Test
+	public void testUpdateScrumRole() {
+		// create test data
+		ProjectObject project = new ProjectObject("TEST_PROJECT_1");
+		project
+		        .setDisplayName("TEST_DISPLATNAME_1")
+		        .setComment("TEST_COMMON_1")
+		        .setManager("TEST_MANAGER")
+		        .setAttachFileSize(2);
+		long projectId = ProjectDAO.getInstance().create(project);
+		assertNotSame(-1, projectId);
+
+		// 從 DB 裡取出 project 的 Scrum role 資料
+		ArrayList<ScrumRole> scrumRoles = new ArrayList<ScrumRole>();
+		for (RoleEnum role : RoleEnum.values()) {
+			scrumRoles.add(ProjectDAO.getInstance().getScrumRole(projectId, "TEST_PROJECT_1", role));
+		}
+
+		assertEquals(5, scrumRoles.size());
+		// assert ProductOwner role
+		assertTrue(scrumRoles.get(0).getAccessProductBacklog());
+		assertTrue(scrumRoles.get(0).getAccessReleasePlan());
+		assertTrue(scrumRoles.get(0).getAccessRetrospective());
+		assertTrue(scrumRoles.get(0).getAccessSprintBacklog());
+		assertTrue(scrumRoles.get(0).getAccessSprintPlan());
+		assertTrue(scrumRoles.get(0).getAccessTaskBoard());
+		assertTrue(scrumRoles.get(0).getAccessUnplannedItem());
+		assertTrue(scrumRoles.get(0).getEditProject());
+		assertTrue(scrumRoles.get(0).getReadReport());
+		
+		// 修改 ProductOwner 的權限
+		scrumRoles.get(0).setAccessProductBacklog(false);
+		scrumRoles.get(0).setAccessReleasePlan(false);
+		scrumRoles.get(0).setAccessRetrospective(false);
+		scrumRoles.get(0).setAccessSprintBacklog(false);
+		scrumRoles.get(0).setAccessSprintPlan(false);
+		scrumRoles.get(0).setAccessTaskBoard(false);
+		scrumRoles.get(0).setAccessUnplannedItem(false);
+		scrumRoles.get(0).setEditProject(false);
+		scrumRoles.get(0).setReadReport(false);
+		ProjectDAO.getInstance().updateScrumRole(projectId, RoleEnum.ProductOwner, scrumRoles.get(0));
+		
+		// assert update data
+		assertFalse(scrumRoles.get(0).getAccessProductBacklog());
+		assertFalse(scrumRoles.get(0).getAccessReleasePlan());
+		assertFalse(scrumRoles.get(0).getAccessRetrospective());
+		assertFalse(scrumRoles.get(0).getAccessSprintBacklog());
+		assertFalse(scrumRoles.get(0).getAccessSprintPlan());
+		assertFalse(scrumRoles.get(0).getAccessTaskBoard());
+		assertFalse(scrumRoles.get(0).getAccessUnplannedItem());
+		assertFalse(scrumRoles.get(0).getEditProject());
+		assertFalse(scrumRoles.get(0).getReadReport());
 	}
 
 	@Test
@@ -233,17 +413,17 @@ public class ProjectDAOTest extends TestCase {
 		IQueryValueSet valueSet = new MySQLQuerySet();
 		valueSet.addTableName(ProjectEnum.TABLE_NAME);
 		String query = valueSet.getSelectQuery();
-		
+
 		ResultSet result = mControl.executeQuery(query);
 		try {
-	        while (result.next()) {
-	        	projects.add(convertProject(result));
-	        }
-        } catch (SQLException e) {
-	        e.printStackTrace();
-        } finally {
-        	closeResultSet(result);
-        }
+			while (result.next()) {
+				projects.add(convertProject(result));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			closeResultSet(result);
+		}
 		assertEquals(2, projects.size());
 	}
 
@@ -251,10 +431,10 @@ public class ProjectDAOTest extends TestCase {
 	public void testGetProjectByName() {
 		// create three test data
 		for (int i = 0; i < 3; i++) {
-			ProjectObject project = new ProjectObject("TEST_PROJECT_" + (i+1));
+			ProjectObject project = new ProjectObject("TEST_PROJECT_" + (i + 1));
 			project
-			        .setDisplayName("TEST_DISPLATNAME_" + (i+1))
-			        .setComment("TEST_COMMON_" + (i+1))
+			        .setDisplayName("TEST_DISPLATNAME_" + (i + 1))
+			        .setComment("TEST_COMMON_" + (i + 1))
 			        .setManager("TEST_MANAGER")
 			        .setAttachFileSize(2);
 			long projectId = ProjectDAO.getInstance().create(project);
@@ -282,53 +462,40 @@ public class ProjectDAOTest extends TestCase {
 			long projectId = ProjectDAO.getInstance().create(project);
 			assertNotSame(-1, projectId);
 		}
-		
+
 		ArrayList<ProjectObject> projects = ProjectDAO.getInstance().getProjects();
-		
+
 		assertEquals(3, projects.size());
 	}
 
-	private ProjectObject convertProject(ResultSet result) {
-		ProjectObject project = null;
-        try {
-	        project = new ProjectObject(result.getLong(ProjectEnum.ID),
-	        		result.getString(ProjectEnum.NAME));
-	        project
-	        .setDisplayName(result.getString(ProjectEnum.DISPLAY_NAME))
-	        .setComment(result.getString(ProjectEnum.COMMENT))
-	        .setManager(result.getString(ProjectEnum.PRODUCT_OWNER))
-	        .setAttachFileSize(result.getLong(ProjectEnum.ATTATCH_MAX_SIZE))
-	        .setCreateTime(result.getLong(ProjectEnum.CREATE_TIME))
-	        .setUpdateTime(result.getLong(ProjectEnum.UPDATE_TIME));
-        } catch (SQLException e) {
-	        e.printStackTrace();
-        } finally {
-        	closeResultSet(result);
-        }
+	private ProjectObject convertProject(ResultSet result) throws SQLException {
+		ProjectObject project = new ProjectObject(result.getLong(ProjectEnum.ID),
+		        result.getString(ProjectEnum.NAME));
+		project
+		        .setDisplayName(result.getString(ProjectEnum.DISPLAY_NAME))
+		        .setComment(result.getString(ProjectEnum.COMMENT))
+		        .setManager(result.getString(ProjectEnum.PRODUCT_OWNER))
+		        .setAttachFileSize(result.getLong(ProjectEnum.ATTATCH_MAX_SIZE))
+		        .setCreateTime(result.getLong(ProjectEnum.CREATE_TIME))
+		        .setUpdateTime(result.getLong(ProjectEnum.UPDATE_TIME));
 		return project;
 	}
-	
-	private ScrumRole convertScrumRole(String projectName, String role, ResultSet result) {
+
+	private ScrumRole convertScrumRole(String projectName, String role, ResultSet result) throws SQLException {
 		ScrumRole scrumRole = new ScrumRole(projectName, role);
 		scrumRole.setisGuest(RoleEnum.Guest == RoleEnum.valueOf(role));
-		try {
-	        scrumRole.setAccessProductBacklog(result.getBoolean(ScrumRoleEnum.ACCESS_PRODUCT_BACKLOG));
-	        scrumRole.setAccessReleasePlan(result.getBoolean(ScrumRoleEnum.ACCESS_RELEASE_PLAN));
-	        scrumRole.setReadReport(result.getBoolean(ScrumRoleEnum.ACCESS_REPORT));
-	        scrumRole.setAccessRetrospective(result.getBoolean(ScrumRoleEnum.ACCESS_RETROSPECTIVE));
-	        scrumRole.setAccessSprintBacklog(result.getBoolean(ScrumRoleEnum.ACCESS_SPRINT_BACKLOG));
-	        scrumRole.setAccessSprintPlan(result.getBoolean(ScrumRoleEnum.ACCESS_SPRINT_PLAN));
-	        scrumRole.setAccessUnplannedItem(result.getBoolean(ScrumRoleEnum.ACCESS_UNPLANNED));
-	        scrumRole.setAccessTaskBoard(result.getBoolean(ScrumRoleEnum.ACCESS_TASKBOARD));
-	        scrumRole.setEditProject(result.getBoolean(ScrumRoleEnum.ACCESS_EDIT_PROJECT));
-        } catch (SQLException e) {
-	        e.printStackTrace();
-        } finally {
-        	closeResultSet(result);
-        }
+		scrumRole.setAccessProductBacklog(result.getBoolean(ScrumRoleEnum.ACCESS_PRODUCT_BACKLOG));
+		scrumRole.setAccessReleasePlan(result.getBoolean(ScrumRoleEnum.ACCESS_RELEASE_PLAN));
+		scrumRole.setReadReport(result.getBoolean(ScrumRoleEnum.ACCESS_REPORT));
+		scrumRole.setAccessRetrospective(result.getBoolean(ScrumRoleEnum.ACCESS_RETROSPECTIVE));
+		scrumRole.setAccessSprintBacklog(result.getBoolean(ScrumRoleEnum.ACCESS_SPRINT_BACKLOG));
+		scrumRole.setAccessSprintPlan(result.getBoolean(ScrumRoleEnum.ACCESS_SPRINT_PLAN));
+		scrumRole.setAccessUnplannedItem(result.getBoolean(ScrumRoleEnum.ACCESS_UNPLANNED));
+		scrumRole.setAccessTaskBoard(result.getBoolean(ScrumRoleEnum.ACCESS_TASKBOARD));
+		scrumRole.setEditProject(result.getBoolean(ScrumRoleEnum.ACCESS_EDIT_PROJECT));
 		return scrumRole;
 	}
-	
+
 	private void closeResultSet(ResultSet result) {
 		if (result != null) {
 			try {
