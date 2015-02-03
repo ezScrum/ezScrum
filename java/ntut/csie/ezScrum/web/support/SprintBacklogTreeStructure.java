108631bca77c066edc284a74456e0cbf1c358399
j99590314 <j99590314@gmail.com>
2015-02-03 16:39:42 +0800

    refactor SprintBacklogTreeStructure, SprintBacklogTreeStructureTest naming and typesetting

--- java/ntut/csie/ezScrum/web/support/SprintBacklogTreeStructure.java
+++ java/ntut/csie/ezScrum/web/support/SprintBacklogTreeStructure.java
@@ -11,30 +11,29 @@ import ntut.csie.ezScrum.web.dataObject.TaskObject;
 
 public class SprintBacklogTreeStructure {
 	// Issue info
-	private String mType = "";
-	private String mIssueId = "";
-	private String mTag = "";
-	private String mName = "";
-	private String mHandler = "";
-	private String mValue = "";
-	private String mEstimate = "";
-	private String mImportance = "";
-	private String mStatus = "";
-	private String mNotes = "";
-	private String mLink = "";
-	private String mSprintID = "";
-	private String mReleaseID = "";
+	String Type = "";
+	String ID = "";
+	String Tag = "";
+	String Name = "";
+	String Handler = "";
+	String Value = "";
+	String Estimate = "";
+	String Importance = "";
+	String Status = "";
+	String Notes = "";
+	String Link = "";
+	String SprintID = "";
+	String ReleaseID = "";
+	// Ext-JS attribute
+	boolean leaf = false;
+	boolean expanded = false;
+	String id = "";
+	String cls = "folder";
+	// Task of Story is a leaf
+	List<SprintBacklogTreeStructure> children = null;
+
 	private ArrayList<Date> mDates = null;
 	private HashMap<String, String> mDateToHourMap = new HashMap<String, String>();
-
-	// Ext-JS attribute
-	private boolean mLeaf = false;
-	private boolean mExpanded = false;
-	private String mId = "";
-	private String mCls = "folder";
-
-	// Task of Story is a leaf
-	private List<SprintBacklogTreeStructure> mChildren = null;
 
 	public SprintBacklogTreeStructure() {
 		// initial empty function
@@ -42,63 +41,62 @@ public class SprintBacklogTreeStructure {
 
 	public SprintBacklogTreeStructure(IIssue story,
 			ArrayList<TaskObject> tasks, ArrayList<Date> dates) {
-		mType = ScrumEnum.STORY_ISSUE_TYPE;
-		mIssueId = Long.toString(story.getIssueID());
-		mName = HandleSpecialChar(story.getSummary());
-		mHandler = " ";
-		mValue = story.getValue();
-		mEstimate = story.getEstimated();
-		mImportance = story.getImportance();
-		mTag = new Translation().Join(story.getTags(), ", ");
-		mStatus = story.getStatus();
-		mNotes = HandleSpecialChar(story.getNotes());
-		mLink = story.getIssueLink();
-		mSprintID = story.getSprintID();
-		mReleaseID = story.getReleaseID();
-		mLeaf = false;
+		Type = ScrumEnum.STORY_ISSUE_TYPE;
+		ID = Long.toString(story.getIssueID());
+		Name = HandleSpecialChar(story.getSummary());
+		Handler = " ";
+		Value = story.getValue();
+		Estimate = story.getEstimated();
+		Importance = story.getImportance();
+		Tag = new Translation().Join(story.getTags(), ", ");
+		Status = story.getStatus();
+		Notes = HandleSpecialChar(story.getNotes());
+		Link = story.getIssueLink();
+		SprintID = story.getSprintID();
+		ReleaseID = story.getReleaseID();
+		leaf = false;
 		// expanded = true;
-		mCls = "folder";
-		mId = Long.toString(story.getIssueID());
-		mChildren = new ArrayList<SprintBacklogTreeStructure>();
+		cls = "folder";
+		id = Long.toString(story.getIssueID());
+		children = new ArrayList<SprintBacklogTreeStructure>();
 		// 設定 Sprint 時間
 		mDates = dates;
 		// 將 child 建構成樹狀
-		TranslaeTaskSturct(tasks);
+		TranslateTaskSturct(tasks);
 	}
 
 	public SprintBacklogTreeStructure(TaskObject task, ArrayList<Date> dates) {
-		mType = ScrumEnum.TASK_ISSUE_TYPE;
-		mIssueId = String.valueOf(task.getId());
-		mName = task.getName();
-		mHandler = task.getHandler().getUsername();
-		mEstimate = String.valueOf(task.getEstimate());
-		mStatus = task.getStatusString();
-		mNotes = task.getNotes();
-		mLeaf = true;
-		mCls = "file";
+		Type = ScrumEnum.TASK_ISSUE_TYPE;
+		ID = String.valueOf(task.getId());
+		Name = task.getName();
+		Handler = task.getHandler().getUsername();
+		Estimate = String.valueOf(task.getEstimate());
+		Status = task.getStatusString();
+		Notes = task.getNotes();
+		leaf = true;
+		cls = "file";
 		mDates = dates;
 	}
-	
+
 	// 根據日期設定 Task 的 Remaining Hours
-		private void SetRemainByDate(TaskObject task) {
-			String value = "";
-			// 使用時間來取得 Remaining Hours 然後用 DateColumns 來當作 Key
-			for (int i = 0; mDates.size() > i; i++) {
-				// 超過今天日期的則不計算
-				if (mDates.get(i).getTime() <= new Date().getTime()) {
-					// remain
-					value = getTaskPoint(mDates.get(i), task);
-				}
-				else {
-					value = "";
-				}
-				// 以日期當作 Key
-				// dateToHourMap.put(currentCols.get(i).GetColumnName(),
-				// value);
-				// 以 TreeGrid Column 當作 Key
-				mDateToHourMap.put("Date_" + String.valueOf(i + 1), value);
+	private void SetRemainByDate(TaskObject task) {
+		String value = "";
+		// 使用時間來取得 Remaining Hours 然後用 DateColumns 來當作 Key
+		for (int i = 0; mDates.size() > i; i++) {
+			// 超過今天日期的則不計算
+			if (mDates.get(i).getTime() <= new Date().getTime()) {
+				// remain
+				value = getTaskPoint(mDates.get(i), task);
+			} else {
+				value = "";
 			}
+			// 以日期當作 Key
+			// this.dateToHourMap.put(this.currentCols.get(i).GetColumnName(),
+			// value);
+			// 以 TreeGrid Column 當作 Key
+			mDateToHourMap.put("Date_" + String.valueOf(i + 1), value);
 		}
+	}
 
 	// 取得當天 Task 的時數
 	private String getTaskPoint(Date date, TaskObject task) {
@@ -112,22 +110,21 @@ public class SprintBacklogTreeStructure {
 	}
 
 	// 將 Task 也轉換成 SprintBacklogTreeStructure 結構
-	private void TranslaeTaskSturct(ArrayList<TaskObject> tasks) {
+	private void TranslateTaskSturct(ArrayList<TaskObject> tasks) {
 		if ((tasks != null) && (tasks.size() > 0)) {
 			for (TaskObject task : tasks) {
 				SprintBacklogTreeStructure taskStructure = new SprintBacklogTreeStructure(
 						task, mDates);
-
 				taskStructure.SetRemainByDate(task);
-				mChildren.add(taskStructure);
+				children.add(taskStructure);
 				taskStructure = null; // release
 			}
 		} else {
-			mChildren = new ArrayList<SprintBacklogTreeStructure>();
+			children = new ArrayList<SprintBacklogTreeStructure>();
 		}
-		// 主要是判斷前端網頁樹狀結構中空story預設要設為展開(因為ext預設是不展開)
-		if (mChildren == null || mChildren.size() == 0) {
-			mExpanded = true;
+		// 主要是判斷前端網頁樹狀結構中空story預設要設為展開(因為 ext 預設是不展開)
+		if (children == null || children.size() == 0) {
+			expanded = true;
 		}
 	}
 
@@ -140,7 +137,7 @@ public class SprintBacklogTreeStructure {
 
 	/*************** For Test ********************/
 	public List<SprintBacklogTreeStructure> GetTasksTreeListForTest() {
-		return mChildren;
+		return children;
 	}
 
 	public HashMap<String, String> GetDatetoRemainMap() {
--- test/ntut/csie/ezScrum/web/support/SprintBacklogTreeStructureTest.java
+++ test/ntut/csie/ezScrum/web/support/SprintBacklogTreeStructureTest.java
@@ -21,9 +21,9 @@ public class SprintBacklogTreeStructureTest {
 	private Element mRoot;
 	private SprintBacklogLogic mSprintBacklogLogic;
 	private Configuration mConfiguration = null;
-	
+
 	@Before
-	protected void setUp(){
+	protected void setUp() {
 		mConfiguration = new Configuration();
 		mConfiguration.setTestMode(true);
 		mConfiguration.save();
@@ -33,180 +33,177 @@ public class SprintBacklogTreeStructureTest {
 		mTask.setCategory(ScrumEnum.TASK_ISSUE_TYPE);
 		mRoot = new Element(ScrumEnum.ROOT_TAG);
 		mSprintBacklogLogic = new SprintBacklogLogic();
-    }
+	}
 
 	@After
-    protected void tearDown(){
-    	mConfiguration.setTestMode(false);
+	protected void tearDown() {
+		mConfiguration.setTestMode(false);
 		mConfiguration.save();
-    	mStory = null;
-    	mTask = null;
-    	mRoot = null;
-    	mSprintBacklogLogic = null;
-    	mConfiguration = null;
-    }
-    
-    /*-----------------------------------------------------------
-	*	測試根據 Task 歷史紀錄取得 Remaining Hours
+		mStory = null;
+		mTask = null;
+		mRoot = null;
+		mSprintBacklogLogic = null;
+		mConfiguration = null;
+	}
+
+	/*-----------------------------------------------------------
+	 *	測試根據 Task 歷史紀錄取得 Remaining Hours
 	-------------------------------------------------------------*/
-    // Sprint Date 沒有跳過假日
+	// Sprint Date 沒有跳過假日
 	@Test
-	public void testRemainingBySprintDaysCase_1()
-	{
+	public void testRemainingBySprintDaysCase_1() {
 		// Remaining Hours History 1
-        Element history_1 = new Element(ScrumEnum.HISTORY_TAG);        
-        history_1.setAttribute(ScrumEnum.ID_HISTORY_ATTR, "20100712171219");
-        Element remains = new Element(ScrumEnum.REMAINS);        
-        remains.setText("5");
-        history_1.addContent(remains);
-        // Remaining Hours History 2
-        Element history_2 = new Element(ScrumEnum.HISTORY_TAG);        
-        history_2.setAttribute(ScrumEnum.ID_HISTORY_ATTR, "20100713171219");
-        Element remains_1 = new Element(ScrumEnum.REMAINS);        
-        remains_1.setText("0");
-        history_2.addContent(remains_1);
-        // 加入 History root
-        mRoot.addContent(history_1);
-        mRoot.addContent(history_2);
-        // 歷史紀錄的時間
-		Date hisDate_1 = DateUtil.dayFillter(history_1
-				.getAttributeValue(ScrumEnum.ID_HISTORY_ATTR),
+		Element history_1 = new Element(ScrumEnum.HISTORY_TAG);
+		history_1.setAttribute(ScrumEnum.ID_HISTORY_ATTR, "20100712171219");
+		Element remains = new Element(ScrumEnum.REMAINS);
+		remains.setText("5");
+		history_1.addContent(remains);
+		// Remaining Hours History 2
+		Element history_2 = new Element(ScrumEnum.HISTORY_TAG);
+		history_2.setAttribute(ScrumEnum.ID_HISTORY_ATTR, "20100713171219");
+		Element remains_1 = new Element(ScrumEnum.REMAINS);
+		remains_1.setText("0");
+		history_2.addContent(remains_1);
+		// 加入 History root
+		mRoot.addContent(history_1);
+		mRoot.addContent(history_2);
+		// 歷史紀錄的時間
+		Date hisDate_1 = DateUtil.dayFillter(
+				history_1.getAttributeValue(ScrumEnum.ID_HISTORY_ATTR),
 				DateUtil._16DIGIT_DATE_TIME_2);
-		Date hisDate_2 = DateUtil.dayFillter(history_2
-				.getAttributeValue(ScrumEnum.ID_HISTORY_ATTR),
+		Date hisDate_2 = DateUtil.dayFillter(
+				history_2.getAttributeValue(ScrumEnum.ID_HISTORY_ATTR),
 				DateUtil._16DIGIT_DATE_TIME_2);
 		// 將歷史紀錄設定至 Task
-        mTask.setTagContent(mRoot);
-        assertEquals("5", mTask.getTagValue(ScrumEnum.REMAINS, hisDate_1));
-        assertEquals("0", mTask.getTagValue(ScrumEnum.REMAINS, hisDate_2));
+		mTask.setTagContent(mRoot);
+		assertEquals("5", mTask.getTagValue(ScrumEnum.REMAINS, hisDate_1));
+		assertEquals("0", mTask.getTagValue(ScrumEnum.REMAINS, hisDate_2));
 
-        mSprintBacklogLogic.calculateSprintBacklogDateList(new Date("2010/07/12"), 5);
-        ArrayList<Date> dateList = mSprintBacklogLogic.getCurrentDateList();
-        SprintBacklogTreeStructure tree = new SprintBacklogTreeStructure(
-							        		mStory, 
-							        		new IIssue[]{ mTask }, 
-							        		dateList);
-        List<SprintBacklogTreeStructure> tasksTree = tree.GetTasksTreeListForTest();
-        for (SprintBacklogTreeStructure taskTree : tasksTree){
-        	assertEquals("5.0", taskTree.GetDatetoRemainMap().get("Date_1"));
-        	assertEquals("0.0", taskTree.GetDatetoRemainMap().get("Date_2"));
-        	assertEquals("0.0", taskTree.GetDatetoRemainMap().get("Date_3"));
-        	assertEquals("0.0", taskTree.GetDatetoRemainMap().get("Date_4"));
-        	assertEquals("0.0", taskTree.GetDatetoRemainMap().get("Date_5"));
-        }
+		mSprintBacklogLogic.calculateSprintBacklogDateList(new Date(
+				"2010/07/12"), 5);
+		ArrayList<Date> dateList = mSprintBacklogLogic.getCurrentDateList();
+		SprintBacklogTreeStructure tree = new SprintBacklogTreeStructure(
+				mStory, new IIssue[] { mTask }, dateList);
+		List<SprintBacklogTreeStructure> tasksTree = tree
+				.GetTasksTreeListForTest();
+		for (SprintBacklogTreeStructure taskTree : tasksTree) {
+			assertEquals("5.0", taskTree.GetDatetoRemainMap().get("Date_1"));
+			assertEquals("0.0", taskTree.GetDatetoRemainMap().get("Date_2"));
+			assertEquals("0.0", taskTree.GetDatetoRemainMap().get("Date_3"));
+			assertEquals("0.0", taskTree.GetDatetoRemainMap().get("Date_4"));
+			assertEquals("0.0", taskTree.GetDatetoRemainMap().get("Date_5"));
+		}
 	}
-	
+
 	// Sprint Date 有跳過假日
 	@Test
-	public void testRemainingBySprintDaysCase_2()
-	{
+	public void testRemainingBySprintDaysCase_2() {
 		// Remaining Hours History 1
-        Element history_1 = new Element(ScrumEnum.HISTORY_TAG);        
-        history_1.setAttribute(ScrumEnum.ID_HISTORY_ATTR, "20100714171219");
-        Element remains = new Element(ScrumEnum.REMAINS);        
-        remains.setText("13");
-        history_1.addContent(remains);
-        // Remaining Hours History 2
-        Element history_2 = new Element(ScrumEnum.HISTORY_TAG);        
-        history_2.setAttribute(ScrumEnum.ID_HISTORY_ATTR, "20100716171219");
-        Element remains_1 = new Element(ScrumEnum.REMAINS);        
-        remains_1.setText("8");
-        history_2.addContent(remains_1);
-        // Remaining Hours History 3
-        Element history_3 = new Element(ScrumEnum.HISTORY_TAG);        
-        history_3.setAttribute(ScrumEnum.ID_HISTORY_ATTR, "20100719171219");
-        Element remains_3 = new Element(ScrumEnum.REMAINS);        
-        remains_3.setText("3");
-        history_3.addContent(remains_3);
-        // Remaining Hours History 4
-        Element history_4 = new Element(ScrumEnum.HISTORY_TAG);        
-        history_4.setAttribute(ScrumEnum.ID_HISTORY_ATTR, "20100720171219");
-        Element remains_4 = new Element(ScrumEnum.REMAINS);        
-        remains_4.setText("0");
-        history_4.addContent(remains_4);
-        // 加入 History root
-        mRoot.addContent(history_1);
-        mRoot.addContent(history_2);
-        mRoot.addContent(history_3);
-        mRoot.addContent(history_4);
-        // 歷史紀錄的時間
-		Date hisDate_1 = DateUtil.dayFillter(history_1
-				.getAttributeValue(ScrumEnum.ID_HISTORY_ATTR),
+		Element history_1 = new Element(ScrumEnum.HISTORY_TAG);
+		history_1.setAttribute(ScrumEnum.ID_HISTORY_ATTR, "20100714171219");
+		Element remains = new Element(ScrumEnum.REMAINS);
+		remains.setText("13");
+		history_1.addContent(remains);
+		// Remaining Hours History 2
+		Element history_2 = new Element(ScrumEnum.HISTORY_TAG);
+		history_2.setAttribute(ScrumEnum.ID_HISTORY_ATTR, "20100716171219");
+		Element remains_1 = new Element(ScrumEnum.REMAINS);
+		remains_1.setText("8");
+		history_2.addContent(remains_1);
+		// Remaining Hours History 3
+		Element history_3 = new Element(ScrumEnum.HISTORY_TAG);
+		history_3.setAttribute(ScrumEnum.ID_HISTORY_ATTR, "20100719171219");
+		Element remains_3 = new Element(ScrumEnum.REMAINS);
+		remains_3.setText("3");
+		history_3.addContent(remains_3);
+		// Remaining Hours History 4
+		Element history_4 = new Element(ScrumEnum.HISTORY_TAG);
+		history_4.setAttribute(ScrumEnum.ID_HISTORY_ATTR, "20100720171219");
+		Element remains_4 = new Element(ScrumEnum.REMAINS);
+		remains_4.setText("0");
+		history_4.addContent(remains_4);
+		// 加入 History root
+		mRoot.addContent(history_1);
+		mRoot.addContent(history_2);
+		mRoot.addContent(history_3);
+		mRoot.addContent(history_4);
+		// 歷史紀錄的時間
+		Date hisDate_1 = DateUtil.dayFillter(
+				history_1.getAttributeValue(ScrumEnum.ID_HISTORY_ATTR),
 				DateUtil._16DIGIT_DATE_TIME_2);
-		Date hisDate_2 = DateUtil.dayFillter(history_2
-				.getAttributeValue(ScrumEnum.ID_HISTORY_ATTR),
+		Date hisDate_2 = DateUtil.dayFillter(
+				history_2.getAttributeValue(ScrumEnum.ID_HISTORY_ATTR),
 				DateUtil._16DIGIT_DATE_TIME_2);
-		Date hisDate_3 = DateUtil.dayFillter(history_3
-				.getAttributeValue(ScrumEnum.ID_HISTORY_ATTR),
+		Date hisDate_3 = DateUtil.dayFillter(
+				history_3.getAttributeValue(ScrumEnum.ID_HISTORY_ATTR),
 				DateUtil._16DIGIT_DATE_TIME_2);
-		Date hisDate_4 = DateUtil.dayFillter(history_4
-				.getAttributeValue(ScrumEnum.ID_HISTORY_ATTR),
+		Date hisDate_4 = DateUtil.dayFillter(
+				history_4.getAttributeValue(ScrumEnum.ID_HISTORY_ATTR),
 				DateUtil._16DIGIT_DATE_TIME_2);
 		// 將歷史紀錄設定至 Task
-        mTask.setTagContent(mRoot);
-        assertEquals("13", mTask.getTagValue(ScrumEnum.REMAINS, hisDate_1));
-        assertEquals("8", mTask.getTagValue(ScrumEnum.REMAINS, hisDate_2));
-        assertEquals("3", mTask.getTagValue(ScrumEnum.REMAINS, hisDate_3));
-        assertEquals("0", mTask.getTagValue(ScrumEnum.REMAINS, hisDate_4));
+		mTask.setTagContent(mRoot);
+		assertEquals("13", mTask.getTagValue(ScrumEnum.REMAINS, hisDate_1));
+		assertEquals("8", mTask.getTagValue(ScrumEnum.REMAINS, hisDate_2));
+		assertEquals("3", mTask.getTagValue(ScrumEnum.REMAINS, hisDate_3));
+		assertEquals("0", mTask.getTagValue(ScrumEnum.REMAINS, hisDate_4));
 
-        mSprintBacklogLogic.calculateSprintBacklogDateList(new Date("2010/07/14"), 5);
-        ArrayList<Date> dateList = mSprintBacklogLogic.getCurrentDateList();
-        SprintBacklogTreeStructure tree = new SprintBacklogTreeStructure(
-							        		mStory, 
-							        		new IIssue[]{ mTask }, 
-							        		dateList);
-        List<SprintBacklogTreeStructure> tasksTree = tree.GetTasksTreeListForTest();
-        for (SprintBacklogTreeStructure taskTree : tasksTree){
-        	assertEquals("13.0", taskTree.GetDatetoRemainMap().get("Date_1"));
-        	assertEquals("13.0", taskTree.GetDatetoRemainMap().get("Date_2"));
-        	assertEquals("8.0", taskTree.GetDatetoRemainMap().get("Date_3"));
-        	assertEquals("3.0", taskTree.GetDatetoRemainMap().get("Date_4"));
-        	assertEquals("0.0", taskTree.GetDatetoRemainMap().get("Date_5"));
-        }
+		mSprintBacklogLogic.calculateSprintBacklogDateList(new Date(
+				"2010/07/14"), 5);
+		ArrayList<Date> dateList = mSprintBacklogLogic.getCurrentDateList();
+		SprintBacklogTreeStructure tree = new SprintBacklogTreeStructure(
+				mStory, new IIssue[] { mTask }, dateList);
+		List<SprintBacklogTreeStructure> tasksTree = tree
+				.GetTasksTreeListForTest();
+		for (SprintBacklogTreeStructure taskTree : tasksTree) {
+			assertEquals("13.0", taskTree.GetDatetoRemainMap().get("Date_1"));
+			assertEquals("13.0", taskTree.GetDatetoRemainMap().get("Date_2"));
+			assertEquals("8.0", taskTree.GetDatetoRemainMap().get("Date_3"));
+			assertEquals("3.0", taskTree.GetDatetoRemainMap().get("Date_4"));
+			assertEquals("0.0", taskTree.GetDatetoRemainMap().get("Date_5"));
+		}
 	}
-	
+
 	// History 沒有 Remain 將以 ESTIMATION 代替
 	@Test
-	public void testRemainingBySprintDaysCase_3()
-	{
+	public void testRemainingBySprintDaysCase_3() {
 		// Remaining Hours History 1
-        Element history_1 = new Element(ScrumEnum.HISTORY_TAG);        
-        history_1.setAttribute(ScrumEnum.ID_HISTORY_ATTR, "20100712171219");
-        Element remains = new Element(ScrumEnum.ESTIMATION);        
-        remains.setText("5");
-        history_1.addContent(remains);
-        // Remaining Hours History 2
-        Element history_2 = new Element(ScrumEnum.HISTORY_TAG);        
-        history_2.setAttribute(ScrumEnum.ID_HISTORY_ATTR, "20100714171219");
-        Element remains_1 = new Element(ScrumEnum.REMAINS);        
-        remains_1.setText("0");
-        history_2.addContent(remains_1);
-        // 加入 History root
-        mRoot.addContent(history_1);
-        mRoot.addContent(history_2);
-        // 歷史紀錄的時間
-		Date hisDate_1 = DateUtil.dayFillter(history_1
-				.getAttributeValue(ScrumEnum.ID_HISTORY_ATTR),
+		Element history_1 = new Element(ScrumEnum.HISTORY_TAG);
+		history_1.setAttribute(ScrumEnum.ID_HISTORY_ATTR, "20100712171219");
+		Element remains = new Element(ScrumEnum.ESTIMATION);
+		remains.setText("5");
+		history_1.addContent(remains);
+		// Remaining Hours History 2
+		Element history_2 = new Element(ScrumEnum.HISTORY_TAG);
+		history_2.setAttribute(ScrumEnum.ID_HISTORY_ATTR, "20100714171219");
+		Element remains_1 = new Element(ScrumEnum.REMAINS);
+		remains_1.setText("0");
+		history_2.addContent(remains_1);
+		// 加入 History root
+		mRoot.addContent(history_1);
+		mRoot.addContent(history_2);
+		// 歷史紀錄的時間
+		Date hisDate_1 = DateUtil.dayFillter(
+				history_1.getAttributeValue(ScrumEnum.ID_HISTORY_ATTR),
 				DateUtil._16DIGIT_DATE_TIME_2);
-		Date hisDate_2 = DateUtil.dayFillter(history_2
-				.getAttributeValue(ScrumEnum.ID_HISTORY_ATTR),
+		Date hisDate_2 = DateUtil.dayFillter(
+				history_2.getAttributeValue(ScrumEnum.ID_HISTORY_ATTR),
 				DateUtil._16DIGIT_DATE_TIME_2);
 		// 將歷史紀錄設定至 Task
-        mTask.setTagContent(mRoot);
-        assertEquals("5", mTask.getTagValue(ScrumEnum.ESTIMATION, hisDate_1));
-        assertEquals("0", mTask.getTagValue(ScrumEnum.REMAINS, hisDate_2));
+		mTask.setTagContent(mRoot);
+		assertEquals("5", mTask.getTagValue(ScrumEnum.ESTIMATION, hisDate_1));
+		assertEquals("0", mTask.getTagValue(ScrumEnum.REMAINS, hisDate_2));
 
-        mSprintBacklogLogic.calculateSprintBacklogDateList(new Date("2010/07/12"), 3);
-        ArrayList<Date> dateList = mSprintBacklogLogic.getCurrentDateList();
-        SprintBacklogTreeStructure tree = new SprintBacklogTreeStructure(
-							        		mStory, 
-							        		new IIssue[]{ mTask }, 
-							        		dateList);
-        List<SprintBacklogTreeStructure> tasksTree = tree.GetTasksTreeListForTest();
-        for (SprintBacklogTreeStructure taskTree : tasksTree){
-        	assertEquals("5.0", taskTree.GetDatetoRemainMap().get("Date_1"));
-        	assertEquals("5.0", taskTree.GetDatetoRemainMap().get("Date_2"));
-        	assertEquals("0.0", taskTree.GetDatetoRemainMap().get("Date_3"));
-        }
+		mSprintBacklogLogic.calculateSprintBacklogDateList(new Date(
+				"2010/07/12"), 3);
+		ArrayList<Date> dateList = mSprintBacklogLogic.getCurrentDateList();
+		SprintBacklogTreeStructure tree = new SprintBacklogTreeStructure(
+				mStory, new IIssue[] { mTask }, dateList);
+		List<SprintBacklogTreeStructure> tasksTree = tree
+				.GetTasksTreeListForTest();
+		for (SprintBacklogTreeStructure taskTree : tasksTree) {
+			assertEquals("5.0", taskTree.GetDatetoRemainMap().get("Date_1"));
+			assertEquals("5.0", taskTree.GetDatetoRemainMap().get("Date_2"));
+			assertEquals("0.0", taskTree.GetDatetoRemainMap().get("Date_3"));
+		}
 	}
 }
