89cfec6db85871a713b8d3862953251eea54ede9
samhuang <imidya@gmail.com>
2015-02-03 11:54:16 +0800

    move md5 hash location

--- java/ntut/csie/ezScrum/restful/mobile/service/LoginWebService.java
+++ java/ntut/csie/ezScrum/restful/mobile/service/LoginWebService.java
@@ -21,10 +21,6 @@ public class LoginWebService {
 		// ezScrum v1.8
 		AccountObject user = null;
 		if (!id.equals(guest) && !password.equals(guest)) {
-			// 從資料庫拿出來的密碼會是加密過的，所以這邊用長度判斷是否已經經過 MD5 加密，如果長度不符合 32 就用 MD5 hash 過一次
-			if (password.length() != 32) {
-				password = AccountDAO.getInstance().getMd5(password);
-			}
 			user = new AccountMapper().confirmAccount(id, password);
 		} else {
 			user = null;
--- test/ntut/csie/ezScrum/web/action/backlog/AddExistedStoryActionTest.java
+++ test/ntut/csie/ezScrum/web/action/backlog/AddExistedStoryActionTest.java
@@ -52,7 +52,8 @@ public class AddExistedStoryActionTest extends MockStrutsTestCase {
 
 		super.setUp();
 
-		setContextDirectory(new File(mConfig.getBaseDirPath() + "/WebContent")); // 設定讀取的 struts-config檔案路徑
+		setContextDirectory(new File(mConfig.getBaseDirPath() + "/WebContent")); // 設定讀取的
+																					// struts-config檔案路徑
 		setServletConfigFile("/WEB-INF/struts-config.xml");
 		setRequestPathInfo("/addExistedStory");
 
@@ -96,11 +97,13 @@ public class AddExistedStoryActionTest extends MockStrutsTestCase {
 	public void testExecute() throws Exception {
 		// ================== set parameter info ====================
 		// 第一筆 Release
-		addRequestParameter("releaseID", Integer.toString(mCreateRelease.getReleaseCount()));
+		addRequestParameter("releaseID",
+				Integer.toString(mCreateRelease.getReleaseCount()));
 		// Sprint 空資料
 		addRequestParameter("sprintID", "");
 
-		String[] storyIds = new String[mCreateProductBacklog.getIssueList().size()];
+		String[] storyIds = new String[mCreateProductBacklog.getIssueList()
+				.size()];
 		int count = 0;
 		for (IIssue issue : mCreateProductBacklog.getIssueList()) {
 			storyIds[count++] = Long.toString(issue.getIssueID());
@@ -109,7 +112,8 @@ public class AddExistedStoryActionTest extends MockStrutsTestCase {
 		addRequestParameter("selects", storyIds);
 
 		// ================ set session info ========================
-		request.getSession().setAttribute("UserSession", mConfig.getUserSession());
+		request.getSession().setAttribute("UserSession",
+				mConfig.getUserSession());
 		request.getSession().setAttribute("Project", mProject);
 
 		// ================ set session info ========================
@@ -127,20 +131,22 @@ public class AddExistedStoryActionTest extends MockStrutsTestCase {
 		// 驗證 ReleasePlan 資料
 		ReleasePlanHelper RPHelper = new ReleasePlanHelper(mProject);
 		IReleasePlanDesc releasePlan = RPHelper.getReleasePlan(Integer
-		        .toString(mCreateRelease.getReleaseCount()));
+				.toString(mCreateRelease.getReleaseCount()));
 		assertNotNull(releasePlan);
-		assertEquals(mCreateRelease.getDefault_RELEASE_NAME(mCreateRelease.getReleaseCount()),
-		        releasePlan.getName());
-		assertEquals(
-		        mCreateRelease.getDefault_RELEASE_DESC(mCreateRelease.getReleaseCount()),
-		        releasePlan.getDescription());
+		assertEquals(mCreateRelease.getDefault_RELEASE_NAME(mCreateRelease
+				.getReleaseCount()), releasePlan.getName());
+		assertEquals(mCreateRelease.getDefault_RELEASE_DESC(mCreateRelease
+				.getReleaseCount()), releasePlan.getDescription());
 
 		// 驗證 Story 有被加入 ReleasePlan ID
-		ProductBacklogHelper PBHelper = new ProductBacklogHelper(mConfig.getUserSession(), mProject);
+		ProductBacklogHelper PBHelper = new ProductBacklogHelper(
+				mConfig.getUserSession(), mProject);
 
 		for (IIssue issue : mCreateProductBacklog.getIssueList()) {
-			String releaseID = PBHelper.getIssue(issue.getIssueID()).getReleaseID();
-			assertEquals(Integer.toString(mCreateRelease.getReleaseCount()), releaseID);
+			String releaseID = PBHelper.getIssue(issue.getIssueID())
+					.getReleaseID();
+			assertEquals(Integer.toString(mCreateRelease.getReleaseCount()),
+					releaseID);
 		}
 
 		// ============= release ==============
@@ -151,57 +157,99 @@ public class AddExistedStoryActionTest extends MockStrutsTestCase {
 	}
 
 	/*
-	 * 待修正: AddExistedStoryAction.java #67 沒有對錯誤 ReleaseID作妥善處理 // 代入錯誤的 ReleaseID 參數 public void testexecuteNoReleaseParameter() throws Exception { // ================ set initial data
-	 * ======================= IProject project = this.CP.getProjectList().get(0); // ================ set initial data =======================
+	 * 待修正: AddExistedStoryAction.java #67 沒有對錯誤 ReleaseID作妥善處理 // 代入錯誤的
+	 * ReleaseID 參數 public void testexecuteNoReleaseParameter() throws Exception
+	 * { // ================ set initial data ======================= IProject
+	 * project = this.CP.getProjectList().get(0); // ================ set
+	 * initial data =======================
 	 * 
-	 * // ================== set parameter info ==================== addRequestParameter("releaseID", "XX"); // 錯誤 ReleaseID addRequestParameter("sprintID", ""); // Sprint 空資料
+	 * // ================== set parameter info ====================
+	 * addRequestParameter("releaseID", "XX"); // 錯誤 ReleaseID
+	 * addRequestParameter("sprintID", ""); // Sprint 空資料
 	 * 
-	 * String[] StoryIDs = new String[this.CPB.getIssueList().size()]; int count = 0; for (IIssue issue : this.CPB.getIssueList()) { StoryIDs[count++] = Long.toString(issue.getIssueID()); }
+	 * String[] StoryIDs = new String[this.CPB.getIssueList().size()]; int count
+	 * = 0; for (IIssue issue : this.CPB.getIssueList()) { StoryIDs[count++] =
+	 * Long.toString(issue.getIssueID()); }
 	 * 
-	 * addRequestParameter("selects", StoryIDs); // ================== set parameter info ====================
+	 * addRequestParameter("selects", StoryIDs); // ================== set
+	 * parameter info ====================
 	 * 
-	 * // ================ set session info ======================== request.getSession().setAttribute("UserSession", config.getUserSession()); request.getSession().setAttribute("Project", project);
-	 * // ================ set session info ======================== request.setHeader("Referer", "?PID=" + project.getName()); // SessionManager 會對URL的參數作分析 ,未帶入此參數無法存入session
+	 * // ================ set session info ========================
+	 * request.getSession().setAttribute("UserSession",
+	 * config.getUserSession()); request.getSession().setAttribute("Project",
+	 * project); // ================ set session info ========================
+	 * request.setHeader("Referer", "?PID=" + project.getName()); //
+	 * SessionManager 會對URL的參數作分析 ,未帶入此參數無法存入session
 	 * 
 	 * actionPerform(); // 執行 action verifyNoActionErrors();
 	 * 
 	 * // 驗證回傳 path verifyForwardPath(null); verifyForward(null);
 	 * 
-	 * // 驗證 ReleasePlan 資料 ReleasePlanHelper helper = new ReleasePlanHelper((IProject) request .getSession().getAttribute("Project")); IReleasePlanDesc ReleasePlan = helper.getReleasePlan(Integer
-	 * .toString(this.CR.getReleaseCount())); assertNotNull(ReleasePlan); assertEquals( this.CR.getDefault_RELEASE_NAME(this.CR.getReleaseCount()), ReleasePlan.getName()); assertEquals(
-	 * this.CR.getDefault_RELEASE_DESC(this.CR.getReleaseCount()), ReleasePlan.getDescription());
+	 * // 驗證 ReleasePlan 資料 ReleasePlanHelper helper = new
+	 * ReleasePlanHelper((IProject) request
+	 * .getSession().getAttribute("Project")); IReleasePlanDesc ReleasePlan =
+	 * helper.getReleasePlan(Integer .toString(this.CR.getReleaseCount()));
+	 * assertNotNull(ReleasePlan); assertEquals(
+	 * this.CR.getDefault_RELEASE_NAME(this.CR.getReleaseCount()),
+	 * ReleasePlan.getName()); assertEquals(
+	 * this.CR.getDefault_RELEASE_DESC(this.CR.getReleaseCount()),
+	 * ReleasePlan.getDescription());
 	 * 
-	 * // 驗證 Story 沒有被加入 ReleasePlan ID ProductBacklogHelper PBhelper = new ProductBacklogHelper(project, config.getUserSession());
+	 * // 驗證 Story 沒有被加入 ReleasePlan ID ProductBacklogHelper PBhelper = new
+	 * ProductBacklogHelper(project, config.getUserSession());
 	 * 
-	 * for (IIssue issue : this.CPB.getIssueList()) { String releaseID = PBhelper.getIssue(issue.getIssueID()).getReleaseID(); assertEquals(Integer.toString(-1), releaseID); }
+	 * for (IIssue issue : this.CPB.getIssueList()) { String releaseID =
+	 * PBhelper.getIssue(issue.getIssueID()).getReleaseID();
+	 * assertEquals(Integer.toString(-1), releaseID); }
 	 * 
-	 * // ============= release ============== project = null; StoryIDs = null; helper = null; ReleasePlan = null; PBhelper = null; }
+	 * // ============= release ============== project = null; StoryIDs = null;
+	 * helper = null; ReleasePlan = null; PBhelper = null; }
 	 */
 
 	/*
-	 * 待修正: AddExistedStoryAction.java #48 沒有對空資料sprintID作妥善處理 // 代入錯誤的 Story 參數 public void testexecuteNoStoryParameter() throws Exception { // ================ set initial data
-	 * ======================= IProject project = this.CP.getProjectList().get(0); // ================ set initial data =======================
+	 * 待修正: AddExistedStoryAction.java #48 沒有對空資料sprintID作妥善處理 // 代入錯誤的 Story 參數
+	 * public void testexecuteNoStoryParameter() throws Exception { //
+	 * ================ set initial data ======================= IProject
+	 * project = this.CP.getProjectList().get(0); // ================ set
+	 * initial data =======================
 	 * 
-	 * // ================== set parameter info ==================== addRequestParameter("releaseID", ""); // Release 空資料 addRequestParameter("sprintID", ""); // Sprint 空資料
+	 * // ================== set parameter info ====================
+	 * addRequestParameter("releaseID", ""); // Release 空資料
+	 * addRequestParameter("sprintID", ""); // Sprint 空資料
 	 * 
-	 * String[] StoryIDs = null; addRequestParameter("selects", StoryIDs); // ================== set parameter info ====================
+	 * String[] StoryIDs = null; addRequestParameter("selects", StoryIDs); //
+	 * ================== set parameter info ====================
 	 * 
-	 * // ================ set session info ======================== request.getSession().setAttribute("UserSession", config.getUserSession()); request.getSession().setAttribute("Project", project);
-	 * // ================ set session info ======================== request.setHeader("Referer", "?PID=" + project.getName()); // SessionManager 會對URL的參數作分析 ,未帶入此參數無法存入session
+	 * // ================ set session info ========================
+	 * request.getSession().setAttribute("UserSession",
+	 * config.getUserSession()); request.getSession().setAttribute("Project",
+	 * project); // ================ set session info ========================
+	 * request.setHeader("Referer", "?PID=" + project.getName()); //
+	 * SessionManager 會對URL的參數作分析 ,未帶入此參數無法存入session
 	 * 
 	 * actionPerform(); // 執行 action verifyNoActionErrors();
 	 * 
 	 * // 驗證回傳 path verifyForwardPath(null); verifyForward(null);
 	 * 
-	 * // 驗證 ReleasePlan 資料 ReleasePlanHelper helper = new ReleasePlanHelper((IProject) request .getSession().getAttribute("Project")); IReleasePlanDesc ReleasePlan = helper.getReleasePlan(Integer
-	 * .toString(this.CR.getReleaseCount())); assertNotNull(ReleasePlan); assertEquals( this.CR.getDefault_RELEASE_NAME(this.CR.getReleaseCount()), ReleasePlan.getName()); assertEquals(
-	 * this.CR.getDefault_RELEASE_DESC(this.CR.getReleaseCount()), ReleasePlan.getDescription());
+	 * // 驗證 ReleasePlan 資料 ReleasePlanHelper helper = new
+	 * ReleasePlanHelper((IProject) request
+	 * .getSession().getAttribute("Project")); IReleasePlanDesc ReleasePlan =
+	 * helper.getReleasePlan(Integer .toString(this.CR.getReleaseCount()));
+	 * assertNotNull(ReleasePlan); assertEquals(
+	 * this.CR.getDefault_RELEASE_NAME(this.CR.getReleaseCount()),
+	 * ReleasePlan.getName()); assertEquals(
+	 * this.CR.getDefault_RELEASE_DESC(this.CR.getReleaseCount()),
+	 * ReleasePlan.getDescription());
 	 * 
-	 * // 驗證 Story 沒有被加入 ReleasePlan ID ProductBacklogHelper PBhelper = new ProductBacklogHelper(project, config.getUserSession());
+	 * // 驗證 Story 沒有被加入 ReleasePlan ID ProductBacklogHelper PBhelper = new
+	 * ProductBacklogHelper(project, config.getUserSession());
 	 * 
-	 * for (IIssue issue : this.CPB.getIssueList()) { String releaseID = PBhelper.getIssue(issue.getIssueID()).getReleaseID(); assertEquals(Integer.toString(-1), releaseID); }
+	 * for (IIssue issue : this.CPB.getIssueList()) { String releaseID =
+	 * PBhelper.getIssue(issue.getIssueID()).getReleaseID();
+	 * assertEquals(Integer.toString(-1), releaseID); }
 	 * 
-	 * // ============= release ============== project = null; StoryIDs = null; helper = null; ReleasePlan = null; PBhelper = null; }
+	 * // ============= release ============== project = null; StoryIDs = null;
+	 * helper = null; ReleasePlan = null; PBhelper = null; }
 	 */
 
 	// -----------------------------------------------------------
@@ -210,14 +258,17 @@ public class AddExistedStoryActionTest extends MockStrutsTestCase {
 	// -------------------------------------------------------------
 	public void testAdd_Story_And_Release_Relation() throws Exception {
 		// 在每個Release中加入一個Sprint
-		AddSprintToRelease addSprint = new AddSprintToRelease(1, mCreateRelease, mCreateProject);
+		AddSprintToRelease addSprint = new AddSprintToRelease(1,
+				mCreateRelease, mCreateProject);
 		addSprint.exe();
 
 		// ================== set parameter info ====================
-		addRequestParameter("releaseID", ""); // Release 為""代表這是將 Story 加入 Sprint 的動作
+		addRequestParameter("releaseID", ""); // Release 為""代表這是將 Story 加入
+												// Sprint 的動作
 		addRequestParameter("sprintID", "1"); // Sprint 空資料
 
-		String[] storyIds = new String[mCreateProductBacklog.getIssueList().size()];
+		String[] storyIds = new String[mCreateProductBacklog.getIssueList()
+				.size()];
 		int count = 0;
 		for (IIssue issue : mCreateProductBacklog.getIssueList()) {
 			storyIds[count++] = Long.toString(issue.getIssueID());
@@ -226,7 +277,8 @@ public class AddExistedStoryActionTest extends MockStrutsTestCase {
 		addRequestParameter("selects", storyIds);
 
 		// ================ set session info ========================
-		request.getSession().setAttribute("UserSession", mConfig.getUserSession());
+		request.getSession().setAttribute("UserSession",
+				mConfig.getUserSession());
 		request.getSession().setAttribute("Project", mProject);
 		// SessionManager 會對 URL 的參數作分析，未帶入此參數無法存入 session
 		request.setHeader("Referer", "?PID=" + mProject.getName());
@@ -242,21 +294,22 @@ public class AddExistedStoryActionTest extends MockStrutsTestCase {
 		// 驗證 ReleasePlan 資料，因為 Sprint 1在 Release 之中，所以 Story 應該也要被加入 Release 中
 		ReleasePlanHelper RPHelper = new ReleasePlanHelper(mProject);
 		IReleasePlanDesc releasePlan = RPHelper.getReleasePlan(Integer
-		        .toString(mCreateRelease.getReleaseCount()));
+				.toString(mCreateRelease.getReleaseCount()));
 		assertNotNull(releasePlan);
-		assertEquals(
-		        mCreateRelease.getDefault_RELEASE_NAME(mCreateRelease.getReleaseCount()),
-		        releasePlan.getName());
-		assertEquals(
-		        mCreateRelease.getDefault_RELEASE_DESC(mCreateRelease.getReleaseCount()),
-		        releasePlan.getDescription());
+		assertEquals(mCreateRelease.getDefault_RELEASE_NAME(mCreateRelease
+				.getReleaseCount()), releasePlan.getName());
+		assertEquals(mCreateRelease.getDefault_RELEASE_DESC(mCreateRelease
+				.getReleaseCount()), releasePlan.getDescription());
 
 		// 驗證 Story 有被加入 ReleasePlan ID
-		ProductBacklogHelper PBhelper = new ProductBacklogHelper(mConfig.getUserSession(), mProject);
+		ProductBacklogHelper PBhelper = new ProductBacklogHelper(
+				mConfig.getUserSession(), mProject);
 
 		for (IIssue issue : mCreateProductBacklog.getIssueList()) {
-			String releaseId = PBhelper.getIssue(issue.getIssueID()).getReleaseID();
-			assertEquals(Integer.toString(mCreateRelease.getReleaseCount()), releaseId);
+			String releaseId = PBhelper.getIssue(issue.getIssueID())
+					.getReleaseID();
+			assertEquals(Integer.toString(mCreateRelease.getReleaseCount()),
+					releaseId);
 		}
 
 		// ============= release ==============
