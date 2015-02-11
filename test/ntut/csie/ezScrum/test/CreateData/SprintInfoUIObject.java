f4042debdbf91fc722c03a1d39a97d4b82ba5c56
j99590314 <j99590314@gmail.com>
2015-02-11 15:08:58 +0800

    refactor SprintInfoUIObject

--- test/ntut/csie/ezScrum/test/CreateData/SprintInfoUIObject.java
+++ test/ntut/csie/ezScrum/test/CreateData/SprintInfoUIObject.java
@@ -1,22 +1,22 @@
 package ntut.csie.ezScrum.test.CreateData;
 
 public class SprintInfoUIObject {
-	public int ID = 0;
-	public String SprintGoal = "";
-	public double CurrentStoryPoint = 0d;
-	public double CurrentTaskPoint = 0d;
-	public String ReleaseID = "Release None";
-	public boolean isCurrentSprint = false;
+	public int mId = 0;
+	public String mSprintGoal = "";
+	public double mCurrentStoryPoint = 0d;
+	public double mCurrentTaskPoint = 0d;
+	public String mReleaseID = "Release None";
+	public boolean mIsCurrentSprint = false;
 	
 	public SprintInfoUIObject() {
 	}
 	
-	public SprintInfoUIObject(int ID, String goal, double sp, double tp, String rid, boolean current) {
-		this.ID = ID;
-		this.SprintGoal = goal;
-		this.CurrentStoryPoint = sp;
-		this.CurrentTaskPoint = tp;
-		this.ReleaseID = "Release #" + rid;
-		this.isCurrentSprint = current;
+	public SprintInfoUIObject(int id, String goal, double sp, double tp, String rid, boolean current) {
+		mId = id;
+		mSprintGoal = goal;
+		mCurrentStoryPoint = sp;
+		mCurrentTaskPoint = tp;
+		mReleaseID = "Release #" + rid;
+		mIsCurrentSprint = current;
 	}
 }
