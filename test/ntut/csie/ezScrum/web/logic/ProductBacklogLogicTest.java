e98362989a813e785b09f14f16fbfe0024b498f7
samhuang <imidya@gmail.com>
2015-04-02 11:43:17 +0800

    ....

--- test/ntut/csie/ezScrum/web/logic/ProductBacklogLogicTest.java
+++ test/ntut/csie/ezScrum/web/logic/ProductBacklogLogicTest.java
@@ -1,12 +1,17 @@
 package ntut.csie.ezScrum.web.logic;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.ArrayList;
 
 import ntut.csie.ezScrum.issue.sql.service.core.Configuration;
+import ntut.csie.ezScrum.iteration.core.IReleasePlanDesc;
 import ntut.csie.ezScrum.refactoring.manager.ProjectManager;
+import ntut.csie.ezScrum.test.CreateData.AddSprintToRelease;
 import ntut.csie.ezScrum.test.CreateData.AddStoryToSprint;
 import ntut.csie.ezScrum.test.CreateData.AddTaskToStory;
 import ntut.csie.ezScrum.test.CreateData.CreateProject;
+import ntut.csie.ezScrum.test.CreateData.CreateRelease;
 import ntut.csie.ezScrum.test.CreateData.CreateSprint;
 import ntut.csie.ezScrum.test.CreateData.InitialSQL;
 import ntut.csie.ezScrum.web.dataObject.ProjectObject;
@@ -14,7 +19,6 @@ import ntut.csie.ezScrum.web.dataObject.StoryObject;
 import ntut.csie.jcis.resource.core.IProject;
 
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertTrue;
@@ -24,8 +28,10 @@ public class ProductBacklogLogicTest {
 	private Configuration mConfig = null;
 	private CreateProject mCP;
 	private CreateSprint mCS;
+	private CreateRelease mCR;
 	private AddStoryToSprint mASTS;
 	private AddTaskToStory mATTS;
+	private AddSprintToRelease mASTR;
 	
     @Before
 	public void setUp() throws Exception {
@@ -38,6 +44,7 @@ public class ProductBacklogLogicTest {
 
 		// create test data
 		int PROJECT_COUNT = 1;
+		int RELEASE_COUNT = 1;
 		int SPRINT_COUNT = 2;
 		int STORY_COUNT = 3;
 		int STORY_ESTIMATE = 5;
@@ -47,9 +54,14 @@ public class ProductBacklogLogicTest {
 
 		mCP = new CreateProject(PROJECT_COUNT);
 		mCP.exeCreate();
-
-		mCS = new CreateSprint(SPRINT_COUNT, mCP);
-		mCS.exe();
+		
+		mCR = new CreateRelease(RELEASE_COUNT, mCP);
+		mCR.exe();
+		
+		mASTR = new AddSprintToRelease(SPRINT_COUNT, mCR, mCP);
+		mASTR.exe();
+		
+		mCS = mASTR.getCreateSprintsList().get(0);
 
 		mASTS = new AddStoryToSprint(STORY_COUNT, STORY_ESTIMATE, mCS, mCP, CREATE_PRODUCTBACKLOG_TYPE);
 		mASTS.exe();
@@ -80,82 +92,83 @@ public class ProductBacklogLogicTest {
 		ini = null;
 		mCP = null;
 		mCS = null;
+		mASTR = null;
 		mASTS = null;
 		mATTS = null;
 	}
 	
 	@Test
 	public void testAddStoriesToSprint() {
-		Assert.assertEquals(3, StoryObject.getStoriesBySprintId(mCS.getSprintIdList().get(0)).size());
-		Assert.assertEquals(3, StoryObject.getStoriesBySprintId(mCS.getSprintIdList().get(1)).size());
-		Assert.assertEquals(1, mASTS.getStories().get(0).getId());
-		Assert.assertEquals(1, mASTS.getStories().get(0).getSprintId());
-		Assert.assertEquals(2, mASTS.getStories().get(1).getId());
-		Assert.assertEquals(1, mASTS.getStories().get(1).getSprintId());
-		Assert.assertEquals(3, mASTS.getStories().get(2).getId());
-		Assert.assertEquals(1, mASTS.getStories().get(2).getSprintId());
-		Assert.assertEquals(4, mASTS.getStories().get(3).getId());
-		Assert.assertEquals(2, mASTS.getStories().get(3).getSprintId());
-		Assert.assertEquals(5, mASTS.getStories().get(4).getId());
-		Assert.assertEquals(2, mASTS.getStories().get(4).getSprintId());
-		Assert.assertEquals(6, mASTS.getStories().get(5).getId());
-		Assert.assertEquals(2, mASTS.getStories().get(5).getSprintId());
+		assertEquals(3, StoryObject.getStoriesBySprintId(mCS.getSprintIdList().get(0)).size());
+		assertEquals(3, StoryObject.getStoriesBySprintId(mCS.getSprintIdList().get(1)).size());
+		assertEquals(1, mASTS.getStories().get(0).getId());
+		assertEquals(1, mASTS.getStories().get(0).getSprintId());
+		assertEquals(2, mASTS.getStories().get(1).getId());
+		assertEquals(1, mASTS.getStories().get(1).getSprintId());
+		assertEquals(3, mASTS.getStories().get(2).getId());
+		assertEquals(1, mASTS.getStories().get(2).getSprintId());
+		assertEquals(4, mASTS.getStories().get(3).getId());
+		assertEquals(2, mASTS.getStories().get(3).getSprintId());
+		assertEquals(5, mASTS.getStories().get(4).getId());
+		assertEquals(2, mASTS.getStories().get(4).getSprintId());
+		assertEquals(6, mASTS.getStories().get(5).getId());
+		assertEquals(2, mASTS.getStories().get(5).getSprintId());
 		ArrayList<Long> storiesId = new ArrayList<Long>();
 		storiesId.add(1l);
 		storiesId.add(2l);
 		storiesId.add(3l);
 		mProductBacklogLogic.addStoriesToSprint(storiesId, mCS.getSprintIdList().get(1));
-		Assert.assertEquals(0, StoryObject.getStoriesBySprintId(mCS.getSprintIdList().get(0)).size());
-		Assert.assertEquals(6, StoryObject.getStoriesBySprintId(mCS.getSprintIdList().get(1)).size());
-		Assert.assertEquals(1, StoryObject.get(mASTS.getStories().get(0).getId()).getId());
-		Assert.assertEquals(2, StoryObject.get(mASTS.getStories().get(0).getId()).getSprintId());
-		Assert.assertEquals(2, StoryObject.get(mASTS.getStories().get(1).getId()).getId());
-		Assert.assertEquals(2, StoryObject.get(mASTS.getStories().get(1).getId()).getSprintId());
-		Assert.assertEquals(3, StoryObject.get(mASTS.getStories().get(2).getId()).getId());
-		Assert.assertEquals(2, StoryObject.get(mASTS.getStories().get(2).getId()).getSprintId());
-		Assert.assertEquals(4, StoryObject.get(mASTS.getStories().get(3).getId()).getId());
-		Assert.assertEquals(2, StoryObject.get(mASTS.getStories().get(3).getId()).getSprintId());
-		Assert.assertEquals(5, StoryObject.get(mASTS.getStories().get(4).getId()).getId());
-		Assert.assertEquals(2, StoryObject.get(mASTS.getStories().get(4).getId()).getSprintId());
-		Assert.assertEquals(6, StoryObject.get(mASTS.getStories().get(5).getId()).getId());
-		Assert.assertEquals(2, StoryObject.get(mASTS.getStories().get(5).getId()).getSprintId());
+		assertEquals(0, StoryObject.getStoriesBySprintId(mCS.getSprintIdList().get(0)).size());
+		assertEquals(6, StoryObject.getStoriesBySprintId(mCS.getSprintIdList().get(1)).size());
+		assertEquals(1, StoryObject.get(mASTS.getStories().get(0).getId()).getId());
+		assertEquals(2, StoryObject.get(mASTS.getStories().get(0).getId()).getSprintId());
+		assertEquals(2, StoryObject.get(mASTS.getStories().get(1).getId()).getId());
+		assertEquals(2, StoryObject.get(mASTS.getStories().get(1).getId()).getSprintId());
+		assertEquals(3, StoryObject.get(mASTS.getStories().get(2).getId()).getId());
+		assertEquals(2, StoryObject.get(mASTS.getStories().get(2).getId()).getSprintId());
+		assertEquals(4, StoryObject.get(mASTS.getStories().get(3).getId()).getId());
+		assertEquals(2, StoryObject.get(mASTS.getStories().get(3).getId()).getSprintId());
+		assertEquals(5, StoryObject.get(mASTS.getStories().get(4).getId()).getId());
+		assertEquals(2, StoryObject.get(mASTS.getStories().get(4).getId()).getSprintId());
+		assertEquals(6, StoryObject.get(mASTS.getStories().get(5).getId()).getId());
+		assertEquals(2, StoryObject.get(mASTS.getStories().get(5).getId()).getSprintId());
 		ArrayList<Long> storiesId2 = new ArrayList<Long>();
 		storiesId2.add(4l);
 		storiesId2.add(5l);
 		storiesId2.add(6l);
 		mProductBacklogLogic.addStoriesToSprint(storiesId2, mCS.getSprintIdList().get(0));
-		Assert.assertEquals(3, StoryObject.getStoriesBySprintId(mCS.getSprintIdList().get(0)).size());
-		Assert.assertEquals(3, StoryObject.getStoriesBySprintId(mCS.getSprintIdList().get(1)).size());
-		Assert.assertEquals(1, StoryObject.get(mASTS.getStories().get(0).getId()).getId());
-		Assert.assertEquals(2, StoryObject.get(mASTS.getStories().get(0).getId()).getSprintId());
-		Assert.assertEquals(2, StoryObject.get(mASTS.getStories().get(1).getId()).getId());
-		Assert.assertEquals(2, StoryObject.get(mASTS.getStories().get(1).getId()).getSprintId());
-		Assert.assertEquals(3, StoryObject.get(mASTS.getStories().get(2).getId()).getId());
-		Assert.assertEquals(2, StoryObject.get(mASTS.getStories().get(2).getId()).getSprintId());
-		Assert.assertEquals(4, StoryObject.get(mASTS.getStories().get(3).getId()).getId());
-		Assert.assertEquals(1, StoryObject.get(mASTS.getStories().get(3).getId()).getSprintId());
-		Assert.assertEquals(5, StoryObject.get(mASTS.getStories().get(4).getId()).getId());
-		Assert.assertEquals(1, StoryObject.get(mASTS.getStories().get(4).getId()).getSprintId());
-		Assert.assertEquals(6, StoryObject.get(mASTS.getStories().get(5).getId()).getId());
-		Assert.assertEquals(1, StoryObject.get(mASTS.getStories().get(5).getId()).getSprintId());
+		assertEquals(3, StoryObject.getStoriesBySprintId(mCS.getSprintIdList().get(0)).size());
+		assertEquals(3, StoryObject.getStoriesBySprintId(mCS.getSprintIdList().get(1)).size());
+		assertEquals(1, StoryObject.get(mASTS.getStories().get(0).getId()).getId());
+		assertEquals(2, StoryObject.get(mASTS.getStories().get(0).getId()).getSprintId());
+		assertEquals(2, StoryObject.get(mASTS.getStories().get(1).getId()).getId());
+		assertEquals(2, StoryObject.get(mASTS.getStories().get(1).getId()).getSprintId());
+		assertEquals(3, StoryObject.get(mASTS.getStories().get(2).getId()).getId());
+		assertEquals(2, StoryObject.get(mASTS.getStories().get(2).getId()).getSprintId());
+		assertEquals(4, StoryObject.get(mASTS.getStories().get(3).getId()).getId());
+		assertEquals(1, StoryObject.get(mASTS.getStories().get(3).getId()).getSprintId());
+		assertEquals(5, StoryObject.get(mASTS.getStories().get(4).getId()).getId());
+		assertEquals(1, StoryObject.get(mASTS.getStories().get(4).getId()).getSprintId());
+		assertEquals(6, StoryObject.get(mASTS.getStories().get(5).getId()).getId());
+		assertEquals(1, StoryObject.get(mASTS.getStories().get(5).getId()).getSprintId());
 	}
 	
 	@Test
 	public void testAddStoriesToSprint_WithInvalidSprintId() {
-		Assert.assertEquals(3, StoryObject.getStoriesBySprintId(mCS.getSprintIdList().get(0)).size());
-		Assert.assertEquals(3, StoryObject.getStoriesBySprintId(mCS.getSprintIdList().get(1)).size());
-		Assert.assertEquals(1, mASTS.getStories().get(0).getId());
-		Assert.assertEquals(1, mASTS.getStories().get(0).getSprintId());
-		Assert.assertEquals(2, mASTS.getStories().get(1).getId());
-		Assert.assertEquals(1, mASTS.getStories().get(1).getSprintId());
-		Assert.assertEquals(3, mASTS.getStories().get(2).getId());
-		Assert.assertEquals(1, mASTS.getStories().get(2).getSprintId());
-		Assert.assertEquals(4, mASTS.getStories().get(3).getId());
-		Assert.assertEquals(2, mASTS.getStories().get(3).getSprintId());
-		Assert.assertEquals(5, mASTS.getStories().get(4).getId());
-		Assert.assertEquals(2, mASTS.getStories().get(4).getSprintId());
-		Assert.assertEquals(6, mASTS.getStories().get(5).getId());
-		Assert.assertEquals(2, mASTS.getStories().get(5).getSprintId());
+		assertEquals(3, StoryObject.getStoriesBySprintId(mCS.getSprintIdList().get(0)).size());
+		assertEquals(3, StoryObject.getStoriesBySprintId(mCS.getSprintIdList().get(1)).size());
+		assertEquals(1, mASTS.getStories().get(0).getId());
+		assertEquals(1, mASTS.getStories().get(0).getSprintId());
+		assertEquals(2, mASTS.getStories().get(1).getId());
+		assertEquals(1, mASTS.getStories().get(1).getSprintId());
+		assertEquals(3, mASTS.getStories().get(2).getId());
+		assertEquals(1, mASTS.getStories().get(2).getSprintId());
+		assertEquals(4, mASTS.getStories().get(3).getId());
+		assertEquals(2, mASTS.getStories().get(3).getSprintId());
+		assertEquals(5, mASTS.getStories().get(4).getId());
+		assertEquals(2, mASTS.getStories().get(4).getSprintId());
+		assertEquals(6, mASTS.getStories().get(5).getId());
+		assertEquals(2, mASTS.getStories().get(5).getSprintId());
 		ArrayList<Long> storiesId = new ArrayList<Long>();
 		storiesId.add(1l);
 		storiesId.add(2l);
@@ -164,31 +177,31 @@ public class ProductBacklogLogicTest {
 		storiesId.add(5l);
 		storiesId.add(6l);
 		mProductBacklogLogic.addStoriesToSprint(storiesId, -1);
-		Assert.assertEquals(3, StoryObject.getStoriesBySprintId(mCS.getSprintIdList().get(0)).size());
-		Assert.assertEquals(3, StoryObject.getStoriesBySprintId(mCS.getSprintIdList().get(1)).size());
-		Assert.assertEquals(1, StoryObject.get(mASTS.getStories().get(0).getId()).getId());
-		Assert.assertEquals(1, StoryObject.get(mASTS.getStories().get(0).getId()).getSprintId());
-		Assert.assertEquals(2, StoryObject.get(mASTS.getStories().get(1).getId()).getId());
-		Assert.assertEquals(1, StoryObject.get(mASTS.getStories().get(1).getId()).getSprintId());
-		Assert.assertEquals(3, StoryObject.get(mASTS.getStories().get(2).getId()).getId());
-		Assert.assertEquals(1, StoryObject.get(mASTS.getStories().get(2).getId()).getSprintId());
-		Assert.assertEquals(4, StoryObject.get(mASTS.getStories().get(3).getId()).getId());
-		Assert.assertEquals(2, StoryObject.get(mASTS.getStories().get(3).getId()).getSprintId());
-		Assert.assertEquals(5, StoryObject.get(mASTS.getStories().get(4).getId()).getId());
-		Assert.assertEquals(2, StoryObject.get(mASTS.getStories().get(4).getId()).getSprintId());
-		Assert.assertEquals(6, StoryObject.get(mASTS.getStories().get(5).getId()).getId());
-		Assert.assertEquals(2, StoryObject.get(mASTS.getStories().get(5).getId()).getSprintId());
+		assertEquals(3, StoryObject.getStoriesBySprintId(mCS.getSprintIdList().get(0)).size());
+		assertEquals(3, StoryObject.getStoriesBySprintId(mCS.getSprintIdList().get(1)).size());
+		assertEquals(1, StoryObject.get(mASTS.getStories().get(0).getId()).getId());
+		assertEquals(1, StoryObject.get(mASTS.getStories().get(0).getId()).getSprintId());
+		assertEquals(2, StoryObject.get(mASTS.getStories().get(1).getId()).getId());
+		assertEquals(1, StoryObject.get(mASTS.getStories().get(1).getId()).getSprintId());
+		assertEquals(3, StoryObject.get(mASTS.getStories().get(2).getId()).getId());
+		assertEquals(1, StoryObject.get(mASTS.getStories().get(2).getId()).getSprintId());
+		assertEquals(4, StoryObject.get(mASTS.getStories().get(3).getId()).getId());
+		assertEquals(2, StoryObject.get(mASTS.getStories().get(3).getId()).getSprintId());
+		assertEquals(5, StoryObject.get(mASTS.getStories().get(4).getId()).getId());
+		assertEquals(2, StoryObject.get(mASTS.getStories().get(4).getId()).getSprintId());
+		assertEquals(6, StoryObject.get(mASTS.getStories().get(5).getId()).getId());
+		assertEquals(2, StoryObject.get(mASTS.getStories().get(5).getId()).getSprintId());
 	}
 	
 	@Test
 	public void testGetExistingStories() {
-		Assert.assertEquals(0, mProductBacklogLogic.getExistingStories().size());
+		assertEquals(0, mProductBacklogLogic.getExistingStories().size());
 		ArrayList<StoryObject> stories = mASTS.getStories();
 		stories.get(1).setSprintId(-1).save();
-		Assert.assertEquals(1, mProductBacklogLogic.getExistingStories().size());
+		assertEquals(1, mProductBacklogLogic.getExistingStories().size());
 		StoryObject addableStory = mProductBacklogLogic.getExistingStories().get(0);
-		Assert.assertEquals(stories.get(1).getId(), addableStory.getId());
-		Assert.assertEquals(-1, addableStory.getSprintId());
+		assertEquals(stories.get(1).getId(), addableStory.getId());
+		assertEquals(-1, addableStory.getSprintId());
 	}
 	
 	@Test
@@ -209,7 +222,25 @@ public class ProductBacklogLogicTest {
 	
 	@Test
 	public void testGetStoriesByRelease() {
+		IReleasePlanDesc release = mCR.getReleaseList().get(0);
+		ArrayList<StoryObject> stories = mProductBacklogLogic.getStoriesByRelease(release);
+		assertEquals(6, stories.size());
 		
+		// remove one of stories from sprint
+		mASTS.getStories().get(0).setSprintId(-1).save();
+		stories = mProductBacklogLogic.getStoriesByRelease(release);
+		assertEquals(5, stories.size());
+		
+		// set first story's importance to 0, should be sorted to last position
+		StoryObject story = mASTS.getStories().get(5);
+		long expectId = story.getId();
+		story.setImportance(0).save();
+		
+		
+		stories = mProductBacklogLogic.getStoriesByRelease(release);
+		assertEquals(5, stories.size());
+		story = stories.get(0);
+		assertEquals(expectId, story.getId());
 	}
 	
 	@Test
