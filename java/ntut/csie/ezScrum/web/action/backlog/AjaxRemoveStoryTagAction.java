52db4283ef52a49a09c3758a1f9366e5a2f394bd
samhuang <imidya@gmail.com>
2015-04-13 14:58:20 +0800

    finish AjaxRemoveStoryTagAction

--- java/ntut/csie/ezScrum/web/action/backlog/AjaxRemoveStoryTagAction.java
+++ java/ntut/csie/ezScrum/web/action/backlog/AjaxRemoveStoryTagAction.java
@@ -3,11 +3,10 @@ package ntut.csie.ezScrum.web.action.backlog;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import ntut.csie.ezScrum.pic.core.IUserSession;
 import ntut.csie.ezScrum.web.action.PermissionAction;
+import ntut.csie.ezScrum.web.dataObject.ProjectObject;
 import ntut.csie.ezScrum.web.helper.ProductBacklogHelper;
 import ntut.csie.ezScrum.web.support.SessionManager;
-import ntut.csie.jcis.resource.core.IProject;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -16,7 +15,7 @@ import org.apache.struts.action.ActionMapping;
 
 public class AjaxRemoveStoryTagAction extends PermissionAction {
 	private static Log log = LogFactory.getLog(AjaxRemoveStoryTagAction.class);
-	
+
 	@Override
 	public boolean isValidAction() {
 		return super.getScrumRole().getAccessProductBacklog();
@@ -32,29 +31,14 @@ public class AjaxRemoveStoryTagAction extends PermissionAction {
 	public StringBuilder getResponse(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response) {
 		log.info("Remove Story Tag in AjaxRemoveStoryTagAction.");
-		// get session info
-		IProject project = (IProject) SessionManager.getProject(request);
-		IUserSession session = (IUserSession) request.getSession().getAttribute("UserSession");
-		
+		ProjectObject project = SessionManager.getProjectObject(request);
+
 		// get parameter info
-		String storyId = request.getParameter("storyId");
+		long storyId = Long.parseLong(request.getParameter("storyId"));
 		long tagId = Long.parseLong(request.getParameter("tagId"));
-		
-		
-		StringBuilder result = (new ProductBacklogHelper(session, project)).getRemoveStoryTagResponseText(storyId,tagId);
+
+		StringBuilder result = new ProductBacklogHelper(project)
+				.getRemoveStoryTagResponseText(storyId, tagId);
 		return result;
-		
-////		ITSPrefsStorage prefs = new ITSPrefsStorage(project, session);
-////		String mantisUrl = prefs.getServerUrl();
-//		ProductBacklogHelper PBHelper = new ProductBacklogHelper(project, session);
-//		
-//		PBHelper.removeStoryTag(storyId,tagId);
-//		IIssue issue = PBHelper.getIssue(Long.parseLong(storyId));
-//		
-//		StringBuilder result = new StringBuilder("");
-////		result.append(new Translation(mantisUrl).translateStoryToJson(issue));
-//		result.append(new Translation().translateStoryToJson(issue));
-//
-//		return result;
 	}
 }
