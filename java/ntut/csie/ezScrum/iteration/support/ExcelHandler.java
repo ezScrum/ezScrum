92c163972dfc99351a11f1a36f66adf3975519c4
ibmboy19 <ibmboy19@gmail.com>
2015-04-23 16:54:43 +0800

    refactor ExcelHandler

--- java/ntut/csie/ezScrum/iteration/support/ExcelHandler.java
+++ java/ntut/csie/ezScrum/iteration/support/ExcelHandler.java
@@ -1,7 +1,6 @@
 package ntut.csie.ezScrum.iteration.support;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import jxl.Cell;
@@ -10,14 +9,9 @@ import jxl.write.Label;
 import jxl.write.WritableSheet;
 import jxl.write.WriteException;
 import jxl.write.biff.RowsExceededException;
-import ntut.csie.ezScrum.iteration.core.ScrumEnum;
-import ntut.csie.ezScrum.web.dataInfo.StoryInfo;
 import ntut.csie.ezScrum.web.dataObject.StoryObject;
 import ntut.csie.ezScrum.web.dataObject.TagObject;
-import ntut.csie.jcis.core.util.DateUtil;
 import ntut.csie.jcis.core.util.FormCheckUtil;
-
-import org.jdom.Element;
 
 public class ExcelHandler {
 	private String FORMTYPE_NAME = "Name";
@@ -40,12 +34,13 @@ public class ExcelHandler {
 	public void load() {
 		mColumns = mSheet.getColumns();
 		mRows = mSheet.getRows();
-		if (!checkTitle()) {
-		} else {
-			if (mRows > 1)
+		
+		if (checkTitle()) {
+			if (mRows > 1) {
 				checkStories();
-			else
+			} else {
 				stories = new ArrayList<StoryObject>();
+			}
 		}
 	}
 
@@ -55,28 +50,19 @@ public class ExcelHandler {
 		int index = 1;
 		for (StoryObject story : stories) {
 			try {
-				((WritableSheet) mSheet).addCell(new Label(0, index, String
-						.valueOf(story.getId())));
+				((WritableSheet) mSheet).addCell(new Label(0, index, String.valueOf(story.getId())));
 				// tag is a list, so we translate it to a string
 				String result = Join(story.getTags(), ",");
 				((WritableSheet) mSheet).addCell(new Label(1, index, result));
-				((WritableSheet) mSheet).addCell(new Label(2, index, story
-						.getName()));
+				((WritableSheet) mSheet).addCell(new Label(2, index, story.getName()));
 				((WritableSheet) mSheet).addCell(new Label(3, index, ""));
-				((WritableSheet) mSheet).addCell(new Label(4, index, String
-						.valueOf(story.getSprintId())));
-				((WritableSheet) mSheet).addCell(new Label(5, index, String
-						.valueOf(story.getValue())));
-				((WritableSheet) mSheet).addCell(new Label(6, index, String
-						.valueOf(story.getImportance())));
-				((WritableSheet) mSheet).addCell(new Label(7, index, String
-						.valueOf(story.getEstimate())));
-				((WritableSheet) mSheet).addCell(new Label(8, index, story
-						.getStatusString()));
-				((WritableSheet) mSheet).addCell(new Label(9, index, story
-						.getNotes()));
-				((WritableSheet) mSheet).addCell(new Label(10, index, story
-						.getHowToDemo()));
+				((WritableSheet) mSheet).addCell(new Label(4, index, String.valueOf(story.getSprintId())));
+				((WritableSheet) mSheet).addCell(new Label(5, index, String.valueOf(story.getValue())));
+				((WritableSheet) mSheet).addCell(new Label(6, index, String.valueOf(story.getImportance())));
+				((WritableSheet) mSheet).addCell(new Label(7, index, String.valueOf(story.getEstimate())));
+				((WritableSheet) mSheet).addCell(new Label(8, index, story.getStatusString()));
+				((WritableSheet) mSheet).addCell(new Label(9, index, story.getNotes()));
+				((WritableSheet) mSheet).addCell(new Label(10, index, story.getHowToDemo()));
 			} catch (RowsExceededException e) {
 				e.printStackTrace();
 			} catch (WriteException e) {
@@ -87,13 +73,15 @@ public class ExcelHandler {
 	}
 
 	private String Join(List<TagObject> tags, String delimiter) {
-		if (tags.isEmpty())
+		if (tags.isEmpty()) {
 			return new String("");
+		}
 
 		StringBuilder sb = new StringBuilder();
 
-		for (TagObject x : tags)
+		for (TagObject x : tags) {
 			sb.append(x.getName() + delimiter);
+		}
 
 		sb.delete(sb.length() - delimiter.length(), sb.length());
 
@@ -109,14 +97,11 @@ public class ExcelHandler {
 			((WritableSheet) mSheet).addCell(new Label(3, 0, "ReleaseID"));
 			((WritableSheet) mSheet).addCell(new Label(4, 0, "SprintID"));
 			((WritableSheet) mSheet).addCell(new Label(5, 0, FORMTYPE_VALUE));
-			((WritableSheet) mSheet)
-					.addCell(new Label(6, 0, FORMTYPE_IMPORTANCE));
-			((WritableSheet) mSheet)
-					.addCell(new Label(7, 0, FORMTYPE_ESTIMATION));
+			((WritableSheet) mSheet).addCell(new Label(6, 0, FORMTYPE_IMPORTANCE));
+			((WritableSheet) mSheet).addCell(new Label(7, 0, FORMTYPE_ESTIMATION));
 			((WritableSheet) mSheet).addCell(new Label(8, 0, "Status"));
 			((WritableSheet) mSheet).addCell(new Label(9, 0, FORMTYPE_NOTES));
-			((WritableSheet) mSheet)
-					.addCell(new Label(10, 0, FORMTYPE_HOWTODEMO));
+			((WritableSheet) mSheet).addCell(new Label(10, 0, FORMTYPE_HOWTODEMO));
 		} catch (RowsExceededException e) {
 			e.printStackTrace();
 		} catch (WriteException e) {
@@ -148,53 +133,54 @@ public class ExcelHandler {
 		// 一行一行加入至stories
 		for (int i = 1; i < mRows; i++) {
 			Cell[] cells = mSheet.getRow(i);
-			if (!isBothNull(cells)) {
+			if (!isContentAllNull(cells)) {
 				StoryObject story = new StoryObject(mProjectId);
 				for (int j = 0; j < mColumns; j++) {
 					Cell titleCell = mSheet.getCell(j, 0);
 					String title = titleCell.getContents();
-					if (title.compareToIgnoreCase(FORMTYPE_NAME) == 0) {
+					boolean isTitleEqualToName = title.compareToIgnoreCase(FORMTYPE_NAME) == 0;
+					boolean isTitleEqualToValue = title.compareToIgnoreCase(FORMTYPE_VALUE) == 0;
+					boolean isTitleEqualToImportance = title.compareToIgnoreCase(FORMTYPE_IMPORTANCE) == 0;
+					boolean isTitleEqualToEstimation = title.compareToIgnoreCase(FORMTYPE_ESTIMATION) == 0;
+					boolean isTitleEqualToHowToDemo = title.compareToIgnoreCase(FORMTYPE_HOWTODEMO) == 0;
+					boolean isTitleEqualToNotes = title.compareToIgnoreCase(FORMTYPE_NOTES) == 0;
+					
+					if (isTitleEqualToName) {
 						String name = cells[j].getContents();
-						boolean result = getResult(name,
-								FormCheckUtil.LENGTH128);
+						boolean result = getResult(name, FormCheckUtil.LENGTH128);
 						if (!isNull(name) && result) {
 							story.setName(name);							
 						}
-					} else if (title.compareToIgnoreCase(FORMTYPE_VALUE) == 0) {
+					} else if (isTitleEqualToValue) {
 						String value = cells[j].getContents();
 						if (!isNull(value)) {
-							boolean result = getResult(value,
-									FormCheckUtil.INTEGER);
+							boolean result = getResult(value, FormCheckUtil.INTEGER);
 							if (result) {
 								story.setValue(Integer.parseInt(value));
 							}
 						}
-					} else if (title.compareToIgnoreCase(FORMTYPE_IMPORTANCE) == 0) {
+					} else if (isTitleEqualToImportance) {
 						String importance = cells[j].getContents();
 						if (!isNull(importance)) {
-							boolean result = getResult(importance,
-									FormCheckUtil.INTEGER);
+							boolean result = getResult(importance, FormCheckUtil.INTEGER);
 							if (result) {
 								story.setImportance(Integer.parseInt(importance));
 							}
 						}
-					} else if (title.compareToIgnoreCase(FORMTYPE_ESTIMATION) == 0) {
+					} else if (isTitleEqualToEstimation) {
 						String estimation = cells[j].getContents();
 						if (!isNull(estimation)) {
-							boolean result = getResult(estimation,
-									FormCheckUtil.DIGITAL);
+							boolean result = getResult(estimation, FormCheckUtil.DIGITAL);
 							if (result) {
 								story.setEstimate(Integer.parseInt(estimation));
 							}
 						}
-					} else if (title.compareToIgnoreCase(FORMTYPE_HOWTODEMO) == 0) {
+					} else if (isTitleEqualToHowToDemo) {
 						String howToDemo = cells[j].getContents();
 						if (!isNull(howToDemo)) {
 							story.setHowToDemo(howToDemo);
 						}
-					}
-
-					else if (title.compareToIgnoreCase(FORMTYPE_NOTES) == 0) {
+					} else if (isTitleEqualToNotes) {
 						String notes = cells[j].getContents();
 						if (notes != null) {
 							story.setNotes(notes);
@@ -207,7 +193,7 @@ public class ExcelHandler {
 	}
 
 	public ArrayList<StoryObject> getStories() {
-		return this.stories;
+		return stories;
 	}
 
 	// 判斷value是否符合型態需求
@@ -233,18 +219,16 @@ public class ExcelHandler {
 	}
 
 	// Cell中的content是否全部為空
-	private Boolean isBothNull(Cell[] cells) {
-		int mount = 0;
+	private Boolean isContentAllNull(Cell[] cells) {
+		boolean isAllNull = false;
 		for (int i = 0; i < cells.length; i++) {
 			String contents = cells[i].getContents();
 			if (isNull(contents)) {
-				mount++;
+				isAllNull = true;
+			} else {
+				isAllNull = false;
 			}
 		}
-		if (mount == cells.length)
-			return Boolean.TRUE;
-		else
-			return Boolean.FALSE;
+		return isAllNull;
 	}
-
 }
