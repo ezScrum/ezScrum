c8564d75f6b0e54141b64888d51b483526134cbd
samhuang <imidya@gmail.com>
2015-05-01 16:42:14 +0800

    finish TokenValidator

--- java/ntut/csie/ezScrum/restful/mobile/controller/v2/TokenValidator.java
+++ java/ntut/csie/ezScrum/restful/mobile/controller/v2/TokenValidator.java
@@ -1,5 +1,35 @@
 package ntut.csie.ezScrum.restful.mobile.controller.v2;
 
+import java.security.MessageDigest;
+
+import ntut.csie.ezScrum.web.dataObject.TokenObject;
+
 public class TokenValidator {
 
+	public static boolean verify(long accountId, String clientPublicToken,
+			String clientDisposableToken, long timestamp) {
+		boolean isValidate = false;
+		TokenObject token = TokenObject.getByAccountId(accountId);
+		String disposableToken;
+		try {
+			disposableToken = genDisposable(token.getPublicToken(),
+					token.getPrivateToken(), timestamp);
+		} catch (Exception e) {
+			return false;
+		}
+		if (clientPublicToken.equals(token.getPublicToken())
+				&& clientDisposableToken.equals(disposableToken)) {
+			isValidate = true;
+		}
+		return isValidate;
+	}
+
+	private static String genDisposable(String publicToken,
+			String privateToken, long timestamp) throws Exception {
+		String plainCode = publicToken + privateToken + timestamp;
+		byte[] bytesOfMessage = plainCode.getBytes("UTF-8");
+		MessageDigest md = MessageDigest.getInstance("MD5");
+		byte[] digest = md.digest(bytesOfMessage);
+		return new String(digest);
+	}
 }
---
+++ test/ntut/csie/ezScrum/restful/mobile/controller/v2/TokenValidatorTest.java
@@ -0,0 +1,80 @@
+package ntut.csie.ezScrum.restful.mobile.controller.v2;
+
+import static org.junit.Assert.*;
+
+import java.security.MessageDigest;
+
+import ntut.csie.ezScrum.issue.sql.service.core.Configuration;
+import ntut.csie.ezScrum.refactoring.manager.ProjectManager;
+import ntut.csie.ezScrum.test.CreateData.CreateAccount;
+import ntut.csie.ezScrum.test.CreateData.InitialSQL;
+import ntut.csie.ezScrum.web.dataObject.TokenObject;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TokenValidatorTest {
+	private Configuration mConfig = null;
+	private CreateAccount mCA;
+	private long mAccountId;
+	private TokenObject mToken;
+	
+	@Before
+	public void setUp() throws Exception {
+		mConfig = new Configuration();
+		mConfig.setTestMode(true);
+		mConfig.save();
+
+		InitialSQL ini = new InitialSQL(mConfig);
+		ini.exe();
+
+		mCA = new CreateAccount(1);
+		mCA.exe();
+		
+		mAccountId = mCA.getAccountList().get(0).getId();
+		
+		mToken = new TokenObject(mAccountId);
+		mToken.save();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		InitialSQL ini = new InitialSQL(mConfig);
+		ini.exe();
+		
+		// 刪除外部檔案
+		ProjectManager projectManager = new ProjectManager();
+		projectManager.deleteAllProject();
+
+		// 讓 config 回到  Production 模式
+		mConfig.setTestMode(false);
+		mConfig.save();
+		
+		mConfig = null;
+		mCA = null;
+	}
+	
+	@Test
+	public void testVerify() throws Exception {
+		String publicToken = mToken.getPublicToken();
+		String privateToken = mToken.getPrivateToken();
+		long timestamp = System.currentTimeMillis();
+		String disposableToken = genDisposable(publicToken, privateToken, timestamp);
+		
+		boolean isValidate = TokenValidator.verify(mAccountId, publicToken, disposableToken, timestamp);
+		assertTrue(isValidate);
+		
+		isValidate = TokenValidator.verify(mAccountId, "test", "tttt", timestamp);
+		assertFalse(isValidate);
+	}
+	
+	private static String genDisposable(String publicToken,
+			String privateToken, long timestamp) throws Exception {
+		String plainCode = publicToken + privateToken + timestamp;
+		byte[] bytesOfMessage = plainCode.getBytes("UTF-8");
+		MessageDigest md = MessageDigest.getInstance("MD5");
+		byte[] digest = md.digest(bytesOfMessage);
+		return new String(digest);
+	}
+}
