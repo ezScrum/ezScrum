fc95795bf0497f1fe35680110325dbaf6892f3e2
samhuang <imidya@gmail.com>
2015-06-26 12:53:49 +0800

    add ATTeardown.java

---
+++ test/ATTeardown.java
@@ -0,0 +1,25 @@
+
+
+
+import ntut.csie.ezScrum.issue.sql.service.core.Configuration;
+import ntut.csie.ezScrum.issue.sql.service.tool.internal.MySQLControl;
+import ntut.csie.ezScrum.refactoring.manager.ProjectManager;
+import ntut.csie.ezScrum.test.CreateData.InitialSQL;
+
+import org.junit.Test;
+
+public class ATTeardown {
+
+	private Configuration mConfig = null;
+	
+	@Test
+	public void testCreateAccount() throws Exception {
+		mConfig = new Configuration();
+		InitialSQL ini = new InitialSQL(mConfig);
+		ini.exe();
+		
+		ProjectManager projectManager = new ProjectManager();
+		projectManager.deleteAllProject();
+	}
+
+}
