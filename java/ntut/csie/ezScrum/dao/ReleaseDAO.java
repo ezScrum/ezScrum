8c1adb51970e7fc486f84e9b7e6522b0cd29cd64
Jay-Liao <j99590314@gmail.com>
2015-08-20 10:35:45 +0800

    init releaseDAO

---
+++ java/ntut/csie/ezScrum/dao/ReleaseDAO.java
@@ -0,0 +1,31 @@
+package ntut.csie.ezScrum.dao;
+
+import ntut.csie.ezScrum.web.dataObject.ReleaseObject;
+
+public class ReleaseDAO extends AbstractDAO<ReleaseObject, ReleaseObject>  {
+
+	@Override
+	public long create(ReleaseObject infoObject) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ReleaseObject get(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean update(ReleaseObject dataObject) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(long id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
