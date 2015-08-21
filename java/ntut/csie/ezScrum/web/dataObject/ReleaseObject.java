778158e3bbbb29c6b95522ac029ae6a1d04f59e5
Jay-Liao <j99590314@gmail.com>
2015-08-21 14:42:02 +0800

    ReleaseBacklog 功能移植至 ReleaseObject

--- java/ntut/csie/ezScrum/web/dataObject/ReleaseObject.java
+++ java/ntut/csie/ezScrum/web/dataObject/ReleaseObject.java
@@ -25,27 +25,27 @@ public class ReleaseObject implements IBaseObject {
 
 	private long mCreateTime = 0;
 	private long mUpdateTime = 0;
-	
+
 	public ReleaseObject(long projectId) {
 		mProjectId = projectId;
 	}
-	
+
 	public ReleaseObject(long id, Long serialId, long projectId) {
 		mId = id;
 		mSerialId = serialId;
 		mProjectId = projectId;
 	}
-	
+
 	public ReleaseObject setName(String name) {
 		mName = name;
 		return this;
 	}
-	
+
 	public ReleaseObject setDescription(String description) {
 		mDescription = description;
 		return this;
 	}
-	
+
 	public ReleaseObject setStartDate(String startDateString) {
 		mStartDate = DateUtil.dayFilter(startDateString);
 		return this;
@@ -55,7 +55,7 @@ public class ReleaseObject implements IBaseObject {
 		mDueDate = DateUtil.dayFilter(dueDateString);
 		return this;
 	}
-	
+
 	public ReleaseObject setCreateTime(long createTime) {
 		mCreateTime = createTime;
 		return this;
@@ -65,11 +65,11 @@ public class ReleaseObject implements IBaseObject {
 		mUpdateTime = updateTime;
 		return this;
 	}
-	
+
 	public long getId() {
 		return mId;
 	}
-	
+
 	public long getSerialId() {
 		return mSerialId;
 	}
@@ -77,15 +77,15 @@ public class ReleaseObject implements IBaseObject {
 	public long getProjectId() {
 		return mProjectId;
 	}
-	
+
 	public String getName() {
 		return mName;
 	}
-	
+
 	public String getDescription() {
 		return mDescription;
 	}
-	
+
 	public String getStartDateString() {
 		return DateUtil.formatBySlashForm(mStartDate);
 	}
@@ -93,7 +93,7 @@ public class ReleaseObject implements IBaseObject {
 	public String getDueDateString() {
 		return DateUtil.formatBySlashForm(mDueDate);
 	}
-	
+
 	public long getCreateTime() {
 		return mCreateTime;
 	}
@@ -101,7 +101,7 @@ public class ReleaseObject implements IBaseObject {
 	public long getUpdateTime() {
 		return mUpdateTime;
 	}
-	
+
 	public static ReleaseObject get(long id) {
 		return ReleaseDAO.getInstance().get(id);
 	}
@@ -133,15 +133,17 @@ public class ReleaseObject implements IBaseObject {
 		}
 		return success;
 	}
-	
+
 	public boolean containsSprint(SprintObject sprint) {
 		Date sprintStartDate = DateUtil.dayFilter(sprint.getStartDateString());
 		Date sprintDueDate = DateUtil.dayFilter(sprint.getDueDateString());
-		boolean isContains = sprintStartDate.after(mStartDate) || DateUtils.isSameDay(sprintStartDate, mStartDate);
-		isContains &= sprintDueDate.before(mDueDate) || DateUtils.isSameDay(sprintDueDate, mDueDate);
+		boolean isContains = sprintStartDate.after(mStartDate)
+				|| DateUtils.isSameDay(sprintStartDate, mStartDate);
+		isContains &= sprintDueDate.before(mDueDate)
+				|| DateUtils.isSameDay(sprintDueDate, mDueDate);
 		return isContains;
 	}
-	
+
 	public ArrayList<SprintObject> getSprints() {
 		ArrayList<SprintObject> sprints = new ArrayList<>();
 		ProjectObject project = ProjectObject.get(mProjectId);
@@ -153,7 +155,7 @@ public class ReleaseObject implements IBaseObject {
 		}
 		return sprints;
 	}
-	
+
 	public ArrayList<StoryObject> getStories() {
 		ArrayList<SprintObject> sprints = getSprints();
 		ArrayList<StoryObject> stories = new ArrayList<StoryObject>();
@@ -162,7 +164,35 @@ public class ReleaseObject implements IBaseObject {
 		}
 		return stories;
 	}
-	
+
+	// 取得該日期前已完成的Story數量
+	public double getDoneStoryByDate(Date date) {
+		double count = 0;
+		ArrayList<StoryObject> stories = getStories();
+		for (StoryObject story : stories) {
+			// Story Close的時間
+
+			int status = story.getStatus(date);
+			if (status == StoryObject.STATUS_DONE) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	// 所有story done後，不論何時把story拉到done都把real point設成0
+	public double getReleaseAllStoryDone() {
+		ArrayList<StoryObject> stories = getStories();
+		double count = stories.size();
+		for (StoryObject story : stories) {
+			// story狀態為done的 count就-1
+			if (story.getStatus() == StoryObject.STATUS_DONE) {
+				count--;
+			}
+		}
+		return count;
+	}
+
 	private void doCreate() {
 		mId = ReleaseDAO.getInstance().create(this);
 		reload();
@@ -172,7 +202,7 @@ public class ReleaseObject implements IBaseObject {
 		mUpdateTime = System.currentTimeMillis();
 		ReleaseDAO.getInstance().update(this);
 	}
-	
+
 	private void resetData(ReleaseObject release) {
 		mId = release.getId();
 		mProjectId = release.getProjectId();
@@ -185,12 +215,12 @@ public class ReleaseObject implements IBaseObject {
 		setCreateTime(release.getCreateTime());
 		setUpdateTime(release.getUpdateTime());
 	}
-	
+
 	private boolean exists() {
 		ReleaseObject release = ReleaseDAO.getInstance().get(mId);
 		return release != null;
 	}
-	
+
 	public String toString() {
 		try {
 			return toJSON().toString();
@@ -200,17 +230,17 @@ public class ReleaseObject implements IBaseObject {
 	}
 
 	@Override
-    public JSONObject toJSON() throws JSONException {
+	public JSONObject toJSON() throws JSONException {
 		JSONObject release = new JSONObject();
 		release.put(ReleaseEnum.ID, mId)
-		       .put(ReleaseEnum.PROJECT_ID, mProjectId)
-		       .put(ReleaseEnum.NAME, mName)
-		       .put(ReleaseEnum.DESCRIPTION, mDescription)
-		       .put(ReleaseEnum.START_DATE, getStartDateString())
-		       .put(ReleaseEnum.DUE_DATE, getDueDateString())
-		       .put(ReleaseEnum.CREATE_TIME, mCreateTime)
-		       .put(ReleaseEnum.UPDATE_TIME, mUpdateTime);
-	    return release;
-    }
-	
+				.put(ReleaseEnum.PROJECT_ID, mProjectId)
+				.put(ReleaseEnum.NAME, mName)
+				.put(ReleaseEnum.DESCRIPTION, mDescription)
+				.put(ReleaseEnum.START_DATE, getStartDateString())
+				.put(ReleaseEnum.DUE_DATE, getDueDateString())
+				.put(ReleaseEnum.CREATE_TIME, mCreateTime)
+				.put(ReleaseEnum.UPDATE_TIME, mUpdateTime);
+		return release;
+	}
+
 }
