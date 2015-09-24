495c43e4561ef1b699ae35b854539e5b51e68575
ibmboy19 <ibmboy19@gmail.com>
2015-09-24 16:38:05 +0800

    add AttachFileDAO UT for create

--- test/ntut/csie/ezScrum/dao/AttachFileDAOTest.java
+++ test/ntut/csie/ezScrum/dao/AttachFileDAOTest.java
@@ -1,12 +1,22 @@
 package ntut.csie.ezScrum.dao;
 
+import static org.junit.Assert.assertEquals;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import ntut.csie.ezScrum.issue.sql.service.core.Configuration;
+import ntut.csie.ezScrum.issue.sql.service.core.IQueryValueSet;
+import ntut.csie.ezScrum.issue.sql.service.internal.MySQLQuerySet;
 import ntut.csie.ezScrum.issue.sql.service.tool.internal.MySQLControl;
 import ntut.csie.ezScrum.test.CreateData.InitialSQL;
+import ntut.csie.ezScrum.web.dataObject.AttachFileObject;
+import ntut.csie.ezScrum.web.databaseEnum.AttachFileEnum;
 
 public class AttachFileDAOTest {
 	private MySQLControl mControl = null;
@@ -38,8 +48,48 @@ public class AttachFileDAOTest {
     }
 	
 	@Test
-	public void testCreate() {
+	public void testCreate() throws SQLException {
+		// Test Data
+		String contentType = "text/plain";
+		String fileName = "TEST_ATTACH_FILE_NAME";
+		String filePath = "TEST_ATTACH_FILE_PATH";
+		long issueId = 10;
+		int issueType = 1;
 		
+		// Build AttachFileObject
+		AttachFileObject.Builder attachFileBuilder = new AttachFileObject.Builder();
+		attachFileBuilder.setContentType(contentType)
+		                 .setName(fileName)
+		                 .setIssueId(issueId)
+		                 .setIssueType(issueType)
+		                 .setPath(filePath);
+		AttachFileObject attachFile = attachFileBuilder.build();
+		
+		// Call DAO
+		long attachFileId = AttachFileDAO.getInstance().create(attachFile);
+		
+		Assert.assertNotSame(-1, attachFileId);
+		
+		AttachFileObject attachFileFromDB = null;
+
+		// 從 DB 裡取出 AttachFile 資料
+		IQueryValueSet valueSet = new MySQLQuerySet();
+		valueSet.addTableName(AttachFileEnum.TABLE_NAME);
+		valueSet.addEqualCondition(AttachFileEnum.ID, attachFileId);
+		String query = valueSet.getSelectQuery();
+		ResultSet result = mControl.executeQuery(query);
+		if (result.next()) {
+			attachFileFromDB = AttachFileDAO.convert(result);
+		}
+		closeResultSet(result);
+
+		// Assert
+		assertEquals(attachFileId, attachFileFromDB.getId());
+		assertEquals(contentType, attachFileFromDB.getContentType());
+		assertEquals(fileName, attachFileFromDB.getName());
+		assertEquals(filePath, attachFileFromDB.getPath());
+		assertEquals(issueId, attachFileFromDB.getIssueId());
+		assertEquals(issueType, attachFileFromDB.getIssueType());
 	}
 	
 	@Test
@@ -60,5 +110,15 @@ public class AttachFileDAOTest {
 	@Test
 	public void testDelete() {
 		
+	}
+	
+	private void closeResultSet(ResultSet result) {
+		if (result != null) {
+			try {
+				result.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}			
+		}
 	}
 }
