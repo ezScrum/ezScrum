b769358ac6ef2a3a22b6bc020b972d08077e4202
Jay <j99590314@gmail.com>
2015-12-06 01:21:44 +0800

    Complete StatusTranslatorTest and ResponseFactoryTest

--- test/ntut/csie/ezScrum/restful/dataMigration/support/ResponseFactoryTest.java
+++ test/ntut/csie/ezScrum/restful/dataMigration/support/ResponseFactoryTest.java
@@ -1,13 +1,52 @@
 package ntut.csie.ezScrum.restful.dataMigration.support;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
+import javax.ws.rs.core.Response;
+
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
 import org.junit.Test;
+
+import ntut.csie.ezScrum.restful.dataMigration.jsonEnum.ResponseJSONEnum;
 
 public class ResponseFactoryTest {
 	@Test
-	public void testGetResponse() {
-		// TODO
-		assertTrue("todo", false);
+	public void testGetResponse_WithNullMessageAndNullContent() throws JSONException {
+		Response response = ResponseFactory.getResponse(Response.Status.OK, null, null);
+		String responseJSONString = response.readEntity(String.class);
+		JSONObject responseJSON = new JSONObject(responseJSONString);
+		String message = responseJSON.getString(ResponseJSONEnum.JSON_KEY_MESSAGE);
+		JSONObject contentJSON = responseJSON.getJSONObject(ResponseJSONEnum.JSON_KEY_CONTENT);
+		assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+		assertEquals("", message);
+		assertEquals(new JSONObject().toString(), contentJSON.toString());
+	}
+	
+	@Test
+	public void testGetResponse_WithEmptyMessageAndEmptyContent() throws JSONException {
+		Response response = ResponseFactory.getResponse(Response.Status.OK, "", "");
+		String responseJSONString = response.readEntity(String.class);
+		JSONObject responseJSON = new JSONObject(responseJSONString);
+		String message = responseJSON.getString(ResponseJSONEnum.JSON_KEY_MESSAGE);
+		JSONObject contentJSON = responseJSON.getJSONObject(ResponseJSONEnum.JSON_KEY_CONTENT);
+		assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+		assertEquals("", message);
+		assertEquals(new JSONObject().toString(), contentJSON.toString());
+	}
+	
+	@Test
+	public void testGetResponse() throws JSONException {
+		JSONObject expetedContentJSON = new JSONObject();
+		expetedContentJSON.put("id", 1);
+		expetedContentJSON.put("name", "project01");
+		Response response = ResponseFactory.getResponse(Response.Status.OK, "success", expetedContentJSON.toString());
+		String responseJSONString = response.readEntity(String.class);
+		JSONObject responseJSON = new JSONObject(responseJSONString);
+		String message = responseJSON.getString(ResponseJSONEnum.JSON_KEY_MESSAGE);
+		JSONObject contentJSON = responseJSON.getJSONObject(ResponseJSONEnum.JSON_KEY_CONTENT);
+		assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+		assertEquals("", message);
+		assertEquals(expetedContentJSON.toString(), contentJSON.toString());
 	}
 }
--- test/ntut/csie/ezScrum/restful/dataMigration/support/StatusTranslatorTest.java
+++ test/ntut/csie/ezScrum/restful/dataMigration/support/StatusTranslatorTest.java
@@ -1,19 +1,30 @@
 package ntut.csie.ezScrum.restful.dataMigration.support;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+
+import ntut.csie.ezScrum.web.dataObject.StoryObject;
+import ntut.csie.ezScrum.web.dataObject.TaskObject;
 
 public class StatusTranslatorTest {
 	@Test
 	public void testGetStoryStatus() {
-		// TODO
-		assertTrue("todo", false);
+		assertEquals(-1, StatusTranslator.getStoryStatus(""));
+		assertEquals(-1, StatusTranslator.getStoryStatus(null));
+		assertEquals(StoryObject.STATUS_UNCHECK, StatusTranslator.getStoryStatus("new"));
+		assertEquals(-1, StatusTranslator.getStoryStatus("assigned"));
+		assertEquals(StoryObject.STATUS_DONE, StatusTranslator.getStoryStatus("closed"));
+		assertEquals(-1, StatusTranslator.getStoryStatus("resolved"));
 	}
 	
 	@Test
 	public void testGetTaskStatus() {
-		// TODO
-		assertTrue("todo", false);
+		assertEquals(-1, StatusTranslator.getTaskStatus(""));
+		assertEquals(-1, StatusTranslator.getTaskStatus(null));
+		assertEquals(TaskObject.STATUS_UNCHECK, StatusTranslator.getTaskStatus("new"));
+		assertEquals(TaskObject.STATUS_CHECK, StatusTranslator.getTaskStatus("assigned"));
+		assertEquals(TaskObject.STATUS_DONE, StatusTranslator.getTaskStatus("closed"));
+		assertEquals(-1, StatusTranslator.getTaskStatus("resolved"));
 	}
 }
