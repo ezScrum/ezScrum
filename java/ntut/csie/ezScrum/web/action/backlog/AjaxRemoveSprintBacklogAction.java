27fab687739112513cf1b308dbd94adf7e9ae15a
beku <beku.ftw@gmail.com>
2016-03-17 21:28:46 +0800

    fixed

--- java/ntut/csie/ezScrum/web/action/backlog/AjaxRemoveSprintBacklogAction.java
+++ java/ntut/csie/ezScrum/web/action/backlog/AjaxRemoveSprintBacklogAction.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletResponse;
 import ntut.csie.ezScrum.web.action.PermissionAction;
 import ntut.csie.ezScrum.web.dataObject.ProjectObject;
 import ntut.csie.ezScrum.web.dataObject.SprintObject;
+import ntut.csie.ezScrum.web.dataObject.StoryObject;
 import ntut.csie.ezScrum.web.helper.ProductBacklogHelper;
 import ntut.csie.ezScrum.web.support.SessionManager;
 
@@ -36,22 +37,24 @@ public class AjaxRemoveSprintBacklogAction extends PermissionAction {
 		ProjectObject project = SessionManager.getProject(request);
 		
 		// get parameter info
-		long serialIssueId = Long.parseLong(request.getParameter("issueID"));
+		long serialStoryId = Long.parseLong(request.getParameter("issueID"));
 		
-		SprintObject sprint = SprintObject.get(project.getId(), serialIssueId);
-		long sprintId = -1;
-		if (sprint != null) {
-			sprintId = sprint.getId();
+		StoryObject story = StoryObject.get(project.getId(), serialStoryId);
+		
+		long storyId = -1;
+		if (story != null) {
+			storyId = story.getId();
 		}
+		
 		String result = "";
 		
 		try{
 			ProductBacklogHelper PBHelper = new ProductBacklogHelper(project);
 			
 			// 將 Story 自 Sprint 移除
-			PBHelper.dropStoryFromSprint(sprintId);
+			PBHelper.dropStoryFromSprint(storyId);
 			
-			result = "<DropStory><Result>true</Result><Story><Id>" + sprintId + "</Id></Story></DropStory>";
+			result = "<DropStory><Result>true</Result><Story><Id>" + serialStoryId + "</Id></Story></DropStory>";
 		}catch (Exception e) {
 			result = "<DropStory><Result>false</Result></DropStory>";
 		}
