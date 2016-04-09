f83c426d2c1e53520448dc569e01b68091940d65
afa <c0960707618@gmail.com>
2016-04-09 13:56:33 +0800

    add the SelectTaskWidget.js

---
+++ WebContent/javascript/ezScrumSharedComponent/SelectTaskWindow.js
@@ -0,0 +1,14 @@
+showTheWindow_Sprint: function(panel, sprintID) {
+    	// show from sprint backlog page
+    	
+    	// initial window info
+    	this.notifyPanel = panel;
+    	this.releaseID = '-1';
+    	this.sprintID = sprintID;
+    	this.show();
+    	
+    	// initial grid info
+    	this.ExistedStoryGrid.releaseID = '-1';
+    	this.ExistedStoryGrid.sprintID = sprintID;
+    	this.ExistedStoryGrid.loadDataModel();
+    },
