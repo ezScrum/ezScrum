9c5e02b1c6d236361eb06163808d6effde0ae788
Jay-Liao <j99590314@gmail.com>
2016-05-19 17:28:11 +0800

    Refine AspectJ and ExportStoriesFromProductBacklogAction.java

--- java/ntut/csie/ezScrum/robust/carelessCleanup/aspectj/ExportStoriesFromProductBacklogActionAspectJ.aj
+++ java/ntut/csie/ezScrum/robust/carelessCleanup/aspectj/ExportStoriesFromProductBacklogActionAspectJ.aj
@@ -2,16 +2,14 @@ package ntut.csie.ezScrum.robust.carelessCleanup.aspectj;
 
 import java.io.IOException;
 
-import ntut.csie.ezScrum.web.action.backlog.ExportStoriesFromProductBacklogAction;
-import ntut.csie.ezScrum.robust.aspectj.tool.AspectJSwitch;
-import org.apache.struts.action.ActionForward;
 import jxl.write.WritableWorkbook;
-
+import ntut.csie.ezScrum.robust.aspectj.tool.AspectJSwitch;
+import ntut.csie.ezScrum.web.action.backlog.ExportStoriesFromProductBacklogAction;
 
 public aspect ExportStoriesFromProductBacklogActionAspectJ {
 	pointcut findWrite(WritableWorkbook workbook) : call(void WritableWorkbook.write()) 
 	&& target(workbook)
-    && withincode(ActionForward ExportStoriesFromProductBacklogAction.execute(..));
+    && withincode(void ExportStoriesFromProductBacklogAction.writeDataToTempFile(..));
 	
 	void around(WritableWorkbook workbook) throws IOException : findWrite(workbook){
 		if (AspectJSwitch.getInstance().isSwitchOn("ExportStoriesFromProductBacklogAction")) {
--- java/ntut/csie/ezScrum/web/action/backlog/ExportStoriesFromProductBacklogAction.java
+++ java/ntut/csie/ezScrum/web/action/backlog/ExportStoriesFromProductBacklogAction.java
@@ -1,6 +1,5 @@
 package ntut.csie.ezScrum.web.action.backlog;
 
-import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -10,10 +9,11 @@ import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.UnhandledException;
+import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
-import org.apache.struts.actions.DownloadAction;
 
 import jxl.Workbook;
 import jxl.write.WritableSheet;
@@ -25,50 +25,38 @@ import ntut.csie.ezScrum.web.dataObject.StoryObject;
 import ntut.csie.ezScrum.web.logic.ProductBacklogLogic;
 import ntut.csie.ezScrum.web.support.SessionManager;
 
-public class ExportStoriesFromProductBacklogAction extends DownloadAction {
-	public ActionForward execute(ActionMapping mapping, ActionForm form, javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws IOException {
+public class ExportStoriesFromProductBacklogAction extends Action {
+	public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+			HttpServletResponse response) {
 		ProjectObject project = SessionManager.getProject(request);
 		WritableWorkbook workbook = null;
 		File tempFile = null;
 		ServletOutputStream outputStream = null;
-		DataInputStream inputStream = null;
+		FileInputStream inputStream = null;
 		try {
 			// set the path of the temp file
 			tempFile = File.createTempFile("ezScrumExcel", Long.toString(System.nanoTime()));
-			String tempFilePath = tempFile.getAbsolutePath();
-			// create Excel
-			workbook = Workbook.createWorkbook(new File(tempFilePath));
-			WritableSheet sheet = workbook.createSheet("BACKLOG", 0);
-			// delicate to excel handler
-			ExcelHandler handler = new ExcelHandler(project.getId(), sheet);
-			// get all stories
-			ArrayList<StoryObject> stories = new ProductBacklogLogic(project).getStories();
-			handler.save(stories);
-			// write date to excel
-			workbook.write();
-			// write content of excel for front-end
-			outputStream = response.getOutputStream();
-			int length = 0;
-			byte[] buffer = new byte[10240];
-			inputStream = new DataInputStream(new FileInputStream(tempFile));
-			while (inputStream != null) { 
-				length = inputStream.read(buffer);
-				// Break when buffer is empty
-				if (length == -1) {
-					break;
-				}
-				outputStream.write(buffer, 0, length);    
-			}
-			response.setContentType("application/vnd.ms-excel");
-			response.setContentLength((int)tempFile.length());
+			writeDataToTempFile(project, workbook, tempFile);
+
+			String contentType = "application/vnd.ms-excel";
+			// Read the file from server and stream it
+			response.setContentType(contentType);
+			response.setContentLength((int) tempFile.length());
 			response.setHeader("Content-disposition", "inline; filename=" + project.getName() + "_ProductBacklog.xls");
+
+			outputStream = response.getOutputStream();
+			inputStream = new FileInputStream(tempFile);
+			int bufferSize = 4096;
+			byte[] outputBuffer = new byte[bufferSize];
+			while (inputStream.read(outputBuffer, 0, bufferSize) != -1) {
+				outputStream.write(outputBuffer, 0, bufferSize);
+			}
 			outputStream.flush();
-		} catch (IOException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
-			throw e;
+			throw new UnhandledException(e);
 		} finally {
 			// release resource
-			ResourceManager.closeResource(workbook);
 			ResourceManager.closeResource(inputStream);
 			ResourceManager.closeResource(outputStream);
 			if (tempFile != null) {
@@ -78,9 +66,24 @@ public class ExportStoriesFromProductBacklogAction extends DownloadAction {
 		return null;
 	}
 
-	@Override
-	protected StreamInfo getStreamInfo(ActionMapping arg0, ActionForm arg1, HttpServletRequest arg2,
-			HttpServletResponse arg3) throws Exception {
-		return null;
+	public void writeDataToTempFile(ProjectObject project, WritableWorkbook workbook, File tempFile) {
+		try {
+			// 建立 Excel
+			workbook = Workbook.createWorkbook(tempFile);
+			WritableSheet sheet = workbook.createSheet("BACKLOG", 0);
+			// delicate to excel handler
+			ExcelHandler handler = new ExcelHandler(project.getId(), sheet);
+			// get all stories
+			ArrayList<StoryObject> stories = new ProductBacklogLogic(project).getStories();
+			handler.save(stories);
+
+			// 寫入excel
+			workbook.write();
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new UnhandledException(e);
+		} finally {
+			ResourceManager.closeResource(workbook);
+		}
 	}
 }
