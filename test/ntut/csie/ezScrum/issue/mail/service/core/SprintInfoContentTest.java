8501f046fb8d8a411f80afdd472cdbb43af7c5a7
afa <c0960707618@gmail.com>
2016-06-19 17:11:43 +0800

    finish

--- test/ntut/csie/ezScrum/issue/mail/service/core/SprintInfoContentTest.java
+++ test/ntut/csie/ezScrum/issue/mail/service/core/SprintInfoContentTest.java
@@ -97,11 +97,43 @@ public class SprintInfoContentTest {
 	}
 
 	@Test
+	public void testGetResult_withImportance() {
+		String expect = "<SprintInfo><subject>ezScrum: Sprint 1 Sprint Info</subject><sprintGoal>TEST_SPRINTGOAL_1</sprintGoal>";
+		expect = expect + "<storyInfo>	TEST_STORY_3(5)\n	TEST_STORY_2(5)\n	TEST_STORY_1(5)";
+		expect = expect + "\nEstimated velocity : 15.0 story points</storyInfo><schedule>	 Sprint period :";
+		
+		ArrayList<StoryObject> stories = mASTS.getStories();
+		int importance = 80;
+		for (StoryObject story : stories) {
+			story.setImportance(importance);
+			story.save();
+			importance += 5;
+		}
+		Calendar cal = Calendar.getInstance();
+		Date mToday = cal.getTime();
+		String startDate = "";
+		String endDate = "";
+		SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");
+		startDate = format.format(mToday);
+		cal.add(Calendar.DAY_OF_YEAR, 13);
+		endDate = format.format(cal.getTime());
+		expect = expect + startDate + " to " + endDate + "\n";
+		expect = expect + "	 Daily Scrum : TEST_SPRINTDAILYINFO_1\n";
+		expect = expect + "	 Sprint demo : " + endDate + " Lab1321</schedule></SprintInfo>";
+		
+		ArrayList<SprintObject> sprints = mCS.getSprints();
+		ArrayList<ProjectObject> projects = mCP.getAllProjects();
+		StringBuilder result = sprintInfoContent.getResult(sprints.get(0), projects.get(0));
+		String resultString = result.toString();
+		assertEquals(expect, resultString);
+	}
+	
+	@Test
 	public void testGetStoryInfo_withImportance() {
 		String expect = "	TEST_STORY_3(5)\n	TEST_STORY_2(5)\n	TEST_STORY_1(5)\n";
 		expect = expect + "Estimated velocity : 15.0 story points";
+		
 		ArrayList<StoryObject> stories = mASTS.getStories();
-
 		int importance = 80;
 		for (StoryObject story : stories) {
 			story.setImportance(importance);
