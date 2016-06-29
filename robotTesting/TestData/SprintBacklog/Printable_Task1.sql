ecc26c43e3825a39a82ee45bcdc58f3ebb82c051
afa <c0960707618@gmail.com>
2016-06-29 10:23:47 +0800

    fix printable task

--- robotTesting/TestData/SprintBacklog/Printable_Task1.sql
+++ robotTesting/TestData/SprintBacklog/Printable_Task1.sql
@@ -264,7 +264,7 @@ CREATE TABLE IF NOT EXISTS `sprint` (
 DELETE FROM `sprint`;
 /*!40000 ALTER TABLE `sprint` DISABLE KEYS */;
 INSERT INTO `sprint` (`id`, `serial_id`, `goal`, `interval`, `team_size`, `available_hours`, `focus_factor`, `start_date`, `end_date`, `demo_date`, `demo_place`, `daily_info`, `project_id`, `create_time`, `update_time`) VALUES
-	(1, 1, 'Sprint01', 2, 4, 100, 100, '2016-06-17 00:00:00', '2016-06-30 00:00:00', '2016-06-30 00:00:00', '', '', 1, 1466136159721, 1466136159721);
+	(1, 1, 'Sprint01', 2, 4, 100, 100, NOW(), NOW() + INTERVAL 13 DAY, NOW() + INTERVAL 13 DAY, '', '', 1, 1466136159721, 1466136159721);
 /*!40000 ALTER TABLE `sprint` ENABLE KEYS */;
 
 
