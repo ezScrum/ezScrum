e0b0d4362f3c08c19713601c254f07f8efbe5571
Ouluyu <rize7410@gmail.com>
2017-04-18 21:02:04 +0800

    Modify release ezScrum

--- ServiceWrapper/ServiceWrapper_Ubuntu/shutDown.sh
+++ ServiceWrapper/ServiceWrapper_Ubuntu/shutDown.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
-ezScrumServisePID="$(ps aux | grep 'java -jar start.jar' | awk '{ print $2 }' | head -n 1)"
+ezScrumServisePID="$(ps aux | grep 'java -jar start_ezScrum.jar' | awk '{ print $2 }' | head -n 1)"
 echo "$ezScrumServisePID"
 sudo kill -9 "$ezScrumServisePID"
