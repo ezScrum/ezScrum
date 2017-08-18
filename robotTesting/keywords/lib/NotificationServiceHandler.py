34be8d4ed8765cd98a372898b0aee5dbe1dbd0c7
Ouluyu <rize7410@gmail.com>
2017-08-18 18:01:33 +0800

    Make AT can run Notification service.

--- robotTesting/keywords/lib/NotificationServiceHandler.py
+++ robotTesting/keywords/lib/NotificationServiceHandler.py
@@ -4,10 +4,7 @@ from subprocess import call
 from subprocess import Popen, PIPE, STDOUT
 
 def NotificationTurnOn():
-    np = subprocess.Popen(['C:/Program Files/Java/jdk1.8.0_77/bin/java.exe','-jar','Microservice/NotificationService/notificationService-1.0.jar'])
+    np = subprocess.Popen(['java','-jar','Microservice/NotificationService/notificationService-1.0.jar'])
 
 def NotificationTurnOff():
     np = subprocess.Popen(['taskkill','/F','/IM','java.exe'])
-
-def testA():
-    subprocess.call(['C:/Program Files/Java/jdk1.8.0_77/bin/java.exe','-jar','Microservice/NotificationService/test.jar'])
